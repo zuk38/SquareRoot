767285727069f5d998f8ff8cf9fad2d85e622637
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-03-14 12:53:00 +0100

    Lambda done

--- amplify/backend/function/addUserToGroup/src/index.js
+++ amplify/backend/function/addUserToGroup/src/index.js
@@ -4,15 +4,15 @@ exports.handler = function afterConfirmationTrigger(event, context, callback) {
     apiVersion: "2016-04-18",
   });
 
-  // the user attribute 'custom:ManagerID' was set on User Sign Up.  Here, we are
+  // the user attribute 'custom:role' was set on User Sign Up.  Here, we are
   // using it as a flag. Depending on its value, add to a specified group.
 
-  let groupName = event.request.userAttributes["custom:ManagerID"];
-  if (groupName.equals("Real Estate Developer")) {
+  let groupName = event.request.userAttributes["custom:role"];
+  if (groupName === "Real Estate Developer") {
     groupName = "REDs";
-  } else if (groupName.equals("Landscape Architect")) {
+  } else if (groupName === "Landscape Architect") {
     groupName = "LARKs";
-  } else if (groupName.equals("Landscape Entrepreneur")) {
+  } else if (groupName === "Landscape Entrepreneur") {
     groupName = "LENTs";
   }
 
