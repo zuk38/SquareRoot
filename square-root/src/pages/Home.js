8aa12189c4e27b574df9e604d285e778495a831a
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-04-07 14:52:57 +0200

    retrieving city from zip-code initial works

--- square-root/src/pages/Home.js
+++ square-root/src/pages/Home.js
@@ -21,11 +21,48 @@ async function addContact() {
   alert("Mail sent!");
 }
 
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
+
 //zipcode
 const zipState = { zipCode: "", city: "", hZone: "" };
-function findCity() {
-  console.log(zipState.zipCode)
-}
+const findCityFromZip = async () => {
+  console.log(zipState.zipCode);
+
+  const response = await fetch(
+    new URL(
+      "https://api.bring.com/pickuppoint/api/postalCode/NO/getCityAndType/1337.json"
+    ),
+    {
+      method: "get",
+      headers: {
+        "X-MyBring-API-Uid": "kmaciejewska14@gmail.com",
+        "X-MyBring-API-Key": "d61c0e32-3f40-4b0c-addf-84929228aed0",
+        "X-Bring-Client-URL": "https://localhost:3000/",
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true,
+        "Access-Control-Allow-Methods": "GET",
+      },
+    }
+  )
+    .then((response) => {
+      console.log(response);
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Something went wrong ...");
+      }
+    })
+    .catch((error) => console.log(error));
+
+  console.log(response);
+};
 
 //contact form
 const formState = { name: "", email: "", message: "" };
@@ -54,11 +91,13 @@ export default function Home(props) {
               class="input"
               type="text"
               placeholder="Enter your Zip Code"
-              onChange={(e) => updateFormState("zipCode", e.target.value, zipState)}
+              onChange={(e) =>
+                updateFormState("zipCode", e.target.value, zipState)
+              }
             />
           </div>
           <div class="control">
-            <a class="button is-info" onClick={findCity}>
+            <a class="button is-info" onClick={findCityFromZip}>
               <span class="icon is-small">
                 <i class="fas fa-arrow-circle-right fa-lg"></i>
               </span>
@@ -76,21 +115,27 @@ export default function Home(props) {
                 <Form.Label>Name</Form.Label>
                 <Form.Control
                   placeholder="Name"
-                  onChange={(e) => updateFormState("name", e.target.value, formState)}
+                  onChange={(e) =>
+                    updateFormState("name", e.target.value, formState)
+                  }
                 />
               </Form.Group>
               <Form.Group>
                 <Form.Label>Email</Form.Label>
                 <Form.Control
                   placeholder="Email"
-                  onChange={(e) => updateFormState("email", e.target.value, formState)}
+                  onChange={(e) =>
+                    updateFormState("email", e.target.value, formState)
+                  }
                 />
               </Form.Group>
               <Form.Group>
                 <Form.Label>Message</Form.Label>
                 <Form.Control
                   placeholder="Message"
-                  onChange={(e) => updateFormState("message", e.target.value, formState)}
+                  onChange={(e) =>
+                    updateFormState("message", e.target.value, formState)
+                  }
                 />
               </Form.Group>
               <Button onClick={addContact}>Send a message</Button>
