3a662c8cc9fa4a079d5cc80f012c5ca89d950015
gro97 <s344222@oslomet.no>
2021-04-13 22:02:13 +0200

    All_Projects changes

--- square-root/src/pages/User/Projects/All_Projects.js
+++ square-root/src/pages/User/Projects/All_Projects.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react'
 import "./projects.css";
-
-/*<div class="proj_background"> Project</div> */
+import icon from '../images/proj_icon.png'
 
 export default class All_Projects extends Component {
     render() {
         return (
-            <div>
-                hello form single plant 
+            <div class="projects_title">
+            <img class="project-icon" href="../images/proj_icon.png"/> 
+            <h1>MINE PROSJEKTER</h1>
+          
+            <h2>
+                Velg et prosjekt for å legge til eller endre et grøntområde, eller opprett et nytt prosjekt. 
+            </h2>
+            
+
             </div>
         )
     }
