c884705ee79373312188dc673b896748b2bb2d41
gro97 <s344222@oslomet.no>
2021-04-17 08:17:36 +0200

    Created vertical menu in User files and Modal on /Projects

--- square-root/src/pages/User/Projects/All_Projects.js
+++ square-root/src/pages/User/Projects/All_Projects.js
@@ -1,108 +1,111 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import "./projects.css";
 import icon from '../../../images/proj_icon.png';
 import Modal from 'react-modal';
 
 const customStyles = {
-    content : {
-      top                   : '50%',
-      left                  : '50%',
-      right                 : 'auto',
-      bottom                : 'auto',
-      marginRight           : '-50%',
-      transform             : 'translate(-50%, -50%)',
-      width                 : '70%',
-      height                : '70%'
+    content: {
+        top: '50%',
+        left: '50%',
+        right: 'auto',
+        bottom: 'auto',
+        marginRight: '-50%',
+        transform: 'translate(-50%, -50%)',
+        width: '70%',
+        height: '70%',
     }
-  };
+};
 
-export default class All_Projects extends Component { 
+export default class All_Projects extends Component {
     state = {
         modalOpen: false
     };
 
     openModal = () => {
-        this.setState({
-            modalOpen: true,
-        });
+        this.setState({modalOpen: true});
     }
 
     closeModal = () => {
-        this.setState({
-            modalOpen: false,
-        });
+        this.setState({modalOpen: false});
     }
 
     render() {
-        return (    
-        <div class="row">
+        return (
 
-           {/* <div class="column left"/> */}
-            <div class="projects-title column right">
-                <img class="project-icon" src={icon} alt="Prosjektikon"/> 
+
+            <div class="projects-title">
+                <img class="p-icon-main"
+                    src={icon}
+                    alt="Prosjektikon"/>
                 <h1>PROSJEKTER</h1>
-                <button class="btn-projects" onClick={this.openModal}><i class="fas fa-plus"></i>NYTT PROSJEKT</button>
-               
-               <div class="modal"> 
-               <Modal
-                    isOpen={this.state.modalOpen}
-                    onRequestClose={this.closeModal}
-                    style={customStyles}
-                    contentLabel="Example Modal"
-                    >
+                <button class="btn-projects"
+                    onClick={
+                        this.openModal
+                }>
+                    <i class="fas fa-plus"></i>NYTT PROSJEKT</button>
 
-                    <h1>La oss lage et økosystem</h1>
-                    <br/>
-                    <h2>Fortell oss litt mer om prosjektet</h2>
-                    <button onClick={this.closeModal}>close</button>
-                    <div>I am a modal</div>
-                    <form>
-                        <input />
-                        <button>tab navigation</button>
-                        <button>stays</button>
-                        <button>inside</button>
-                        <button>the modal</button>
-                    </form>
-        </Modal>
-        </div>
-                
+                <div class="modal">
+                    <Modal isOpen={
+                            this.state.modalOpen
+                        }
+                        onRequestClose={
+                            this.closeModal
+                        }
+                        style={customStyles}
+                        contentLabel="Example Modal"
+                        >
+
+                        <button onClick={
+                                this.closeModal
+                            }
+                            class="btn-modal-close" alt="Lukk">
+                            <i class="fas fa-times fa-lg"></i>
+                        </button>
+
+
+                        <h1>La oss lage et økosystem</h1>
+                        <br/>
+                        <h2>Fortell oss litt mer om prosjektet</h2>
+
+                        <div>I am a modal</div>
+                        <form>
+                            <input/>
+                            <button>tab navigation</button>
+                            <button>stays</button>
+                            <button>inside</button>
+                            <button>the modal</button>
+                        </form>
+                    </Modal>
+                </div>
+
                 <br></br>
-                <br></br>
-                
                 <h2>Velg et prosjekt for å legge til eller endre et grøntområde, eller opprett et nytt prosjekt.</h2>
-    
-           
-            
-            {/*TABLE must GET data from GraphQL*/}
-            <table id="projects-table">
-                <tr>
-                    <th>PROSJEKTNAVN</th>
-                    <th>ADRESSE</th>
-                    <th>BESKRIVELSE</th>
-                    <th>OPPRETTET</th>
-                </tr>
-                <tr>
-                    <td>Sørengkaia</td>
-                    <td>Sørengkaia 63, 0166 Oslo</td>
-                    <td>OBOS</td>
-                    <td>04.03.2021</td>
-                </tr>
-                <tr>
-                    <td>Liertoppen</td>
-                    <td>Lierveien 3, 1657 Lier</td>
-                    <td>Privat</td>
-                    <td>12.02.2021</td>
-                </tr>
-            </table>
+
+
+                {/*TABLE must GET data from GraphQL*/}
+                <table id="projects-table">
+                    <tr>
+                        <th>PROSJEKTNAVN</th>
+                        <th>ADRESSE</th>
+                        <th>BESKRIVELSE</th>
+                        <th>OPPRETTET</th>
+                    </tr>
+                    <tr>
+                        <td>Sørengkaia</td>
+                        <td>Sørengkaia 63, 0166 Oslo</td>
+                        <td>OBOS</td>
+                        <td>04.03.2021</td>
+                    </tr>
+                    <tr>
+                        <td>Liertoppen</td>
+                        <td>Lierveien 3, 1657 Lier</td>
+                        <td>Privat</td>
+                        <td>12.02.2021</td>
+                    </tr>
+                </table>
             </div>
-        </div>
+
         )
     }
-    
-    }
 
-
- 
-
-
-
+}
--- square-root/src/pages/User/Projects/Dashboard.js
+++ square-root/src/pages/User/Projects/Dashboard.js
@@ -1,46 +1,48 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import "./projects.css";
 import icon from '../../../images/proj_icon.png';
-import { Menu } from 'semantic-ui-react'
+import {Menu} from 'semantic-ui-react'
 
 
 /*onst logo = require('../../../images/proj_icon')*/
 
-export default class Dashboard extends Component { 
+export default class Dashboard extends Component {
 
     render() {
-    
+
         return (
-           <div class="row">
-            <div class="column left">
-            <div class="menu-vertical">
-                <a href="#" class="active"><i class="fas fa-users"></i>Dashbord</a>
-                <a href="/Members"><i class="fas fa-users"></i>Medlemmer</a>
-                <a href="/Orders"><i class="fas fa-tasks"></i>Bestillinger</a>
-                <a href="/Settings"> <i class="fas fa-cog"></i>Instillinger</a>
-                </div>
-            </div>
-     
-            <div class="projects-title column right">
-                
-                <img class="project-icon" src={icon} alt="Prosjektikon"/> 
-                {/*GET DATA*/}
-                <h1>Project Title</h1>
-                <button class="btn"><i class="fas fa-plus"></i>NYTT PROSJEKT</button>
-                <div id="Modal" class="modal">
-                    <div class="modal-content">
-                        <span class="close">&times;</span>
-                        <p class={FormData}></p>
+            <div class="row">
+                <div class="p-column left">
+                    <a href="/Projects" class="btn-back">
+                        <i class="fas fa-chevron-left"></i>Alle Prosjekter</a>
+                    <div class="menu-vertical">
+                        <a href="#" class="active">
+                            <i class="fas fa-columns"></i>Dashbord</a>
+                        <a href="/Members">
+                            <i class="fas fa-users"></i>Medlemmer</a>
+                        <a href="/Orders">
+                            <i class="fas fa-tasks"></i>Bestillinger</a>
+                        <a href="/Settings">
+                            <i class="fas fa-cog"></i>Innstillinger</a>
                     </div>
                 </div>
-                <br></br>
-                <br></br>
-                <h2>Role: {/*GET DATA*/}</h2>
 
-                
-            </div>
-            </div>
-        
+                <div class="p-column right">
+
+                    <img class="project-icon"
+                        src={icon}
+                        alt="Prosjektikon"/> {/*GET DATA*/}
+                    <h1>Project Title</h1>
+
+                    <br></br>
+                    <br></br>
+                    <h2>Role: {/*GET DATA*/}</h2>
+
+                    <div class="concepts-frame">
+                       {/*GET DATA*/}<img src='../../../images/defaultBackground.png' alt="Bilde av grøntareal"/></div>
+                    </div>
+                </div>
+
         )
     }
 }
