c702f3519985eef94932e00dc1625b94f569106d
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-04-18 04:00:45 +0200

    Added user dropdown component

--- square-root/src/components/Dropdown.js
+++ square-root/src/components/Dropdown.js
@@ -1,32 +1,92 @@
-import React, { useState } from "react";
-import { NavLink } from "react-router-dom";
+import React, { useState, useEffect, useRef } from "react";
+import Auth from "@aws-amplify/auth";
+import { CSSTransition } from "react-transition-group";
+import { ReactComponent as ArrowIcon } from "../icons/arrow.svg";
+import { ReactComponent as LogoutIcon } from "../icons/logout.svg";
+import { ReactComponent as ChevronIcon } from "../icons/chevron.svg";
+import * as FaIcons from "react-icons/fa";
+import { Link } from "react-router-dom";
 import { MenuItems } from "./NavbarData";
 
-export default function Dropdown() {
-  const [click, setClick] = useState(false);
-  const handleClick = () => setClick(!click);
+export default function Dropdown(props) {
+  const [activeMenu, setActiveMenu] = useState("main");
+  const [menuHeight, setMenuHeight] = useState(null);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    setMenuHeight(dropdownRef.current?.firstChild.offsetHeight);
+  }, []);
+
+  function calcHeight(el) {
+    let height = el.offsetHeight;
+    setMenuHeight(height);
+  }
+
+  const handleLogOut = async (event) => {
+    event.preventDefault();
+    try {
+      Auth.signOut();
+      props.auth.setAuthStatus(false);
+      props.auth.setUser(null);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  function DropdownItem(props) {
+    return (
+      <a
+        href="#"
+        className="dropdown-item"
+        onClick={() => props.goToMenu && setActiveMenu(props.goToMenu)}
+      >
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    );
+  }
 
   return (
-    <>
-      <ul
-        onClick={handleClick}
-        className={click ? "dropdown-menu-clicked" : "dropdown-menu"}
+    <div className="dropdown" style={{ height: menuHeight }} ref={dropdownRef}>
+      <CSSTransition
+        in={activeMenu === "main"}
+        timeout={500}
+        classNames="menu-primary"
+        unmountOnExit
+        onEnter={calcHeight}
       >
-        {MenuItems.map((item, index) => {
-          return (
-            <li key={index}>
-              <NavLink
-                className={item.className}
-                to={item.path}
-                exact={true}
-                activeStyle={{ fontWeight: "bold" }}
-              >
-                {item.title}
-              </NavLink>
-            </li>
-          );
-        })}
-      </ul>
-    </>
+        <div className="menu">
+          <DropdownItem
+            leftIcon={<FaIcons.FaUser/>}
+            rightIcon={<ChevronIcon />}
+            goToMenu="profile"
+          >
+            My Profile
+          </DropdownItem>
+
+          <DropdownItem leftIcon={<LogoutIcon />} onClick={handleLogOut}>
+            Log Out
+          </DropdownItem>
+        </div>
+      </CSSTransition>
+
+      <CSSTransition
+        in={activeMenu === "profile"}
+        timeout={500}
+        classNames="menu-secondary"
+        unmountOnExit
+        onEnter={calcHeight}
+      >
+        <div className="menu">
+          <DropdownItem goToMenu="main" leftIcon={<ArrowIcon />}>
+            <h2>My Profile</h2>
+          </DropdownItem>
+          <DropdownItem>My Projects</DropdownItem>
+          <DropdownItem>Account</DropdownItem>
+          <DropdownItem>Help Center</DropdownItem>
+        </div>
+      </CSSTransition>
+    </div>
   );
 }
--- square-root/src/components/Navbar.js
+++ square-root/src/components/Navbar.js
@@ -1,37 +1,18 @@
-import Auth from "@aws-amplify/auth";
 import React, { useState } from "react";
 import logo from "../images/logo.png";
 import * as FaIcons from "react-icons/fa";
 import { Link, NavLink } from "react-router-dom";
-import { MenuItems, NavbarData } from "./NavbarData";
+import { NavbarData } from "./NavbarData";
 import { IconContext } from "react-icons";
 import Backdrop from "./Backdrop";
 import useWindowDimensions from "./hooks/useWindowDimensions";
 import Dropdown from "./Dropdown";
+import { ReactComponent as MenuIcon } from "../icons/menu.svg";
 
 const Navbar = (props) => {
   const [sidebar, setSidebar] = useState(false);
-  const showSideBar = () => setSidebar(!sidebar);
+  const [dropdown, setDropdown] = useState(false);
   let width = useWindowDimensions();
-
-  const [dropdown, setDropdown] = useState(true);
-  const showDropdown = () => {
-    setDropdown(!dropdown);
-    console.log(dropdown);
-  };
-  const [click, setClick] = useState(false);
-  const handleClick = () => setClick(!click);
-
-  const handleLogOut = async (event) => {
-    event.preventDefault();
-    try {
-      Auth.signOut();
-      props.auth.setAuthStatus(false);
-      props.auth.setUser(null);
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
 
   return (
     <IconContext.Provider value={{ color: "#000" }}>
@@ -41,28 +22,7 @@ const Navbar = (props) => {
             <Link to="/">
               <img src={logo} alt="SQUAREROOT" />
             </Link>
-            <div className="user">
-              {!props.auth.isAuthenticated && (
-                <a href="/login" className="button is-outlined">
-                  <span>Sign in</span>
-                  <span class="icon is-small">
-                    <FaIcons.FaUser />
-                  </span>
-                </a>
-              )}
-            </div>
-            {props.auth.isAuthenticated && (
-              <div className="user-logged">
-                <span className="icon-text" onClick={handleLogOut}>
-                  <span>{props.auth.user} </span>
-                  <span class="icon is-medium">
-                    <FaIcons.FaChevronDown />
-                  </span>
-                </span>
-              </div>
-            )}
           </div>
-
           <ul className="nav-links">
             {width > 1190 &&
               NavbarData.slice(
@@ -73,58 +33,97 @@ const Navbar = (props) => {
               ).map((item, index) => {
                 return (
                   <li key={index}>
-                    <Link to={item.path}>{item.title}</Link>
+                    <NavLink
+                      to={item.path}
+                      exact={true}
+                      activeStyle={{ fontWeight: "bold" }}
+                    >
+                      {item.title}
+                    </NavLink>
                   </li>
                 );
               })}
           </ul>
         </div>
-
-        {/*sidebar*/}
-        <Link to="#" className="sidebar-bars" onClick={showSideBar}>
-          {!sidebar && <FaIcons.FaBars />}
-        </Link>
-        {sidebar && <Backdrop />}
-        <div className={sidebar ? "side-menu active" : "side-menu"}>
-          <ul className="side-menu-items" onClick={showSideBar}>
-            <li className="sidebar-toggle">
-              <Link to="#" className="sidebar-bars">
-                <FaIcons.FaTimes />
-              </Link>
+        <ul className="navbar-nav">
+          {/* user */}
+          {!props.auth.isAuthenticated ? (
+            <button href="/login" className="user-round-button">
+              <a href="/login">Sign in</a>
+              <span>
+                <FaIcons.FaUser />
+              </span>
+            </button>
+          ) : (
+            <li className="nav-item">
+              <button
+                class="user-round-button"
+                onClick={() => setDropdown(!dropdown)}
+              >
+                {props.auth.user}
+                <span>
+                  <i class="fas fa-caret-down" />
+                </span>
+              </button>
+              {dropdown && <Dropdown />}
             </li>
-            {width <= 1190
-              ? NavbarData.map((item, index) => {
-                  return (
-                    <li key={index} className={item.className}>
-                      <NavLink
-                        to={item.path}
-                        activeStyle={{ fontWeight: "bold" }}
-                        exact={true}
-                      >
-                        {item.title}
-                      </NavLink>
-                    </li>
-                  );
-                })
-              : NavbarData.slice(
-                  NavbarData.map(function(e) {
-                    return e.title;
-                  }).indexOf("HOME")
-                ).map((item, index) => {
-                  return (
-                    <li key={index} className={item.className}>
-                      <NavLink
-                        to={item.path}
-                        activeStyle={{ fontWeight: "bold" }}
-                        exact={true}
-                      >
-                        {item.title}
-                      </NavLink>
-                    </li>
-                  );
-                })}
-          </ul>
-        </div>
+          )}
+
+          {/*sidebar*/}
+          {sidebar && <Backdrop />}
+          <li className="nav-item">
+            <Link
+              to="#"
+              className="icon-button"
+              onClick={() => setSidebar(!sidebar)}
+            >
+              {!sidebar && <MenuIcon />}
+            </Link>
+          </li>
+          <div className={sidebar ? "side-menu active" : "side-menu"}>
+            <ul
+              className="side-menu-items"
+              onClick={() => setSidebar(!sidebar)}
+            >
+              <li className="sidebar-toggle">
+                <Link to="#" className="sidebar-bars">
+                  <FaIcons.FaTimes />
+                </Link>
+              </li>
+              {width <= 1190
+                ? NavbarData.map((item, index) => {
+                    return (
+                      <li key={index} className={item.className}>
+                        <NavLink
+                          to={item.path}
+                          activeStyle={{ fontWeight: "bold" }}
+                          exact={true}
+                        >
+                          {item.title}
+                        </NavLink>
+                      </li>
+                    );
+                  })
+                : NavbarData.slice(
+                    NavbarData.map(function(e) {
+                      return e.title;
+                    }).indexOf("HOME")
+                  ).map((item, index) => {
+                    return (
+                      <li key={index} className={item.className}>
+                        <NavLink
+                          to={item.path}
+                          activeStyle={{ fontWeight: "bold" }}
+                          exact={true}
+                        >
+                          {item.title}
+                        </NavLink>
+                      </li>
+                    );
+                  })}
+            </ul>
+          </div>
+        </ul>
       </nav>
     </IconContext.Provider>
   );
