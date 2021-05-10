bf373c219fe4e3027857f6a689bc63e6882d638a
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-05-10 10:35:30 +0200

    starting on plant context

---
+++ square-root/src/context/plants.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+const PlantContext = React.createContext();
+
+export default class PlantProvider extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
