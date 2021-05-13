75a8fe47452256a259b99d37fb502c0ca298282e
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-05-13 16:12:20 +0200

    added loading component

---
+++ square-root/src/components/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import loading from '../images/loading.gif'
+
+export default function Loading() {
+    return (
+        <div className="loading">
+            <img src={loading} alt="" />
+        </div>
+    )
+}
