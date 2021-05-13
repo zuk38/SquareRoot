dbb47e59d15f3f706de508e8fcb950860d895ead
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-05-14 00:49:55 +0200

    plantMiniature component

---
+++ square-root/src/components/PlantMiniature.js
@@ -0,0 +1,84 @@
+import React from "react";
+import "../styles/Plants.css";
+import QualityBadge from "../images/quality.png";
+import { ReactComponent as BeeIcon } from "../icons/bee.svg";
+import { ReactComponent as EdibleIcon } from "../icons/eat.svg";
+import { ReactComponent as NativeIcon } from "../icons/norway.svg";
+import { ReactComponent as PetKidsIcon } from "../icons/pets.svg";
+import { ReactComponent as AirIcon } from "../icons/air-purifier.svg";
+import { ReactComponent as SunIcon } from "../icons/sun.svg";
+import PropTypes from 'prop-types';
+
+export default function PlantMiniature({ plant }) {
+  const iconMap = {
+    pollinator_friendly: <BeeIcon />,
+    edible: <EdibleIcon />,
+    pet_kids_friendly: <PetKidsIcon />,
+    air_puryfying: <AirIcon />,
+    sun_seeker: <SunIcon />,
+    native: <NativeIcon />,
+  };
+
+  const {
+    name,
+    img,
+    norwegian_nursery,
+    pollinator_friendly,
+    edible,
+    pet_kids_friendly,
+    air_puryfying,
+    sun_seeker,
+    native,
+  } = plant;
+
+  let tempFeatures = {
+    pollinator_friendly,
+    edible,
+    pet_kids_friendly,
+    air_puryfying,
+    sun_seeker,
+    native,
+  };
+
+  var features = Object.keys(tempFeatures).filter(function(x) {
+    return tempFeatures[x] !== false;
+  });
+  console.log(features);
+
+  return (
+    <article className="plant">
+      <div className="plant-img-container">
+        <img src={img} alt={name} />
+        {norwegian_nursery && (
+          <div className="badge-top">
+            <img src={QualityBadge} />
+          </div>
+        )}
+        <div className="featureList">
+          <div className="featureList-center">
+            {features.map((icon) => (
+              <span className="icon-button">{iconMap[icon]}</span>
+            ))}
+          </div>
+        </div>
+
+        <button class="button is-dark plant-link">FEATURES</button>
+        <p className="plant-info">{name.toUpperCase()}</p>
+      </div>
+    </article>
+  );
+}
+
+PlantMiniature.propTypes = {
+  plant: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    img: PropTypes.string.isRequired,
+    norwegian_nursery: PropTypes.bool.isRequired,
+    pollinator_friendly: PropTypes.bool.isRequired,
+    edible: PropTypes.bool.isRequired,
+    pet_kids_friendly: PropTypes.bool.isRequired,
+    air_puryfying: PropTypes.bool.isRequired,
+    sun_seeker: PropTypes.bool.isRequired,
+    native: PropTypes.bool.isRequired,
+  })
+}
--- square-root/src/pages/Plants.js
+++ square-root/src/pages/Plants.js
@@ -1,211 +1,151 @@
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
-import './Plants.css';
+import React, { Component } from "react";
+import { PlantContext } from "../context/plants";
+import ReactDOM from "react-dom";
+import "../styles/Plants.css";
+import Loading from "../components/Loading";
+import PlantMiniature from "../components/PlantMiniature";
+import Title from "../components/PlantMiniature";
+import QualityBadge from "../images/quality.png";
 
-const plantList = [
-    {
-      "Id":"1001",
-      "name":"Musa",
-      "img": "../../../images/musa.png",
-      "Water Management":"Low",
-      "Functional Requirements":[
-        "Stormwater Management",
-        "Pollinator Friendly"
-      ],
-      "Origin":"Local"
-    },
-    {
-      "Id":"1002",
-      "name":"Pertusem",
-      "img": "../../../images/pertusem.png",
-      "Water Management":"Medium",
-      "Functional Requirements":[
-        "Edible",
-        "Pollinator Friendly"
-      ],
-      "Origin":"Norwegian Nursery"
-    },
-    {
-      "Id":"1003",
-      "name":"Philodendron",
-      "img": "../../../images/philodendron.png",
-      "Water Management":"High",
-      "Functional Requirements":[
-        "Stormwater Management",
-        "Edible",
-        "Pollinator Friendly"
-      ],
-      "Origin":"Local"
-    },
-    {
-        "Id":"1001",
-        "name":"Musa",
-        "img": "../../../images/musa.png",
-        "Water Management":"Low",
-        "Functional Requirements":[
-          "Stormwater Management",
-          "Pollinator Friendly"
-        ],
-        "Origin":"Local"
-      },
-      {
-        "Id":"1002",
-        "name":"Pertusem",
-        "img": "../../../images/pertusem.png",
-        "Water Management":"Medium",
-        "Functional Requirements":[
-          "Edible",
-          "Pollinator Friendly"
-        ],
-        "Origin":"Norwegian Nursery"
-      },
-      {
-        "Id":"1003",
-        "name":"Philodendron",
-        "img": "../../../images/philodendron.png",
-        "Water Management":"High",
-        "Functional Requirements":[
-          "Stormwater Management",
-          "Edible",
-          "Pollinator Friendly"
-        ],
-        "Origin":"Local"
-      }
-  ];
-  
-  const plantFilters = {
-    "Functional Requirements": [
-        "Rain Garden",
-        "Pollinator Friendly",
-        "Edible"
-      ],
-      "Type": [
-        "Flower",
-        "Grass",
-        "Buss & Berry"
-      ],
-      "Origin": [
-        "Native",
-        "Norwegian Nursery"
-      ],
-      "Sun Light": [
-        "Sun Seekers",
-        "Shaddow lovers"
-      ]
-  };
+export default class Plants extends Component {
+  static contextType = PlantContext;
 
-  
-  
-  function Plants () {
-    const [plantFilter, setPlantFilter] = React.useState({
-        "Functional Requirements": [],
-        "Type": [],
-        "Origin": [],
-        "Sun Light": []
-      });
-    
-    const [plants, setPlants] = React.useState([]);
-    const [filteredPlants, setFilteredPlants] = React.useState([]);
-    
-    //Runs at load
-    React.useEffect(() => {
-          setPlants(plantList);
-      setFilteredPlants(plantList);
-      }, []);
-    
-    //Runs when plantFilter is changed
-    React.useEffect(() => {
-          let newPlantList = [];
-      let tempPlants = plants;
-      
-      tempPlants.map(plant => {
-        let matchesFilter = false;
-        
-        Object.keys(plantFilter).map((pf) => {
-          plantFilter[pf].map((pfAlternative) => {
-            if (Array.isArray(plant[pf]) && plant[pf].includes(pfAlternative))
-              matchesFilter = true;
-            else if (plant[pf] == pfAlternative) {
-              matchesFilter = true;
-            }
-          });
-        });
-        
-        if (matchesFilter) {
-          newPlantList.push(plant);
-        }
-      });
-      
-      if (newPlantList.length <= 0)
-        newPlantList = plantList;
-      
-      setFilteredPlants(newPlantList);
-      }, [plantFilter]);
-      
-    //onClick handler for the filter checkboxes
-    const handleFilterChange = (event, pf, pfAlternative) => {
-      let currentFilter = plantFilter;
-                          
-      if (event.target.checked) {
-        currentFilter[pf].push(pfAlternative);
-      } else {
-        currentFilter[pf].splice(currentFilter[pf].indexOf(pfAlternative), 1);
-      }
-      
-      setPlantFilter({...currentFilter});
-    };
-
+  render() {
+    let { loading, plants } = this.context;
+    plants = plants.map((plant) => {
+      return <PlantMiniature key={plant.Id} plant={plant} />;
+    });
 
     return (
-      <div>
-         
-
-    <div className="customize-content">
-<div className="cust-concept-title"><h1>PLANTER</h1>     
-            <p>I Oslo anbefales det med biologisk mangfoldige planter og temperaturregulerende planter.
-Vi kan skrive mer her for å gi mer informasjon.</p></div>
-          </div>
-        <div className="filters">
-          {
-            Object.keys(plantFilters).map((pf) => { 
-              return <div>
-                <b className="pf">{pf}</b>
-                {
-                  plantFilters[pf].map((pfAlternative) => {
-                    return <label>
-                      <input type="checkbox" 
-                        key={pfAlternative} 
-                        value={pfAlternative}
-                        onClick={(event) => handleFilterChange(event, pf, pfAlternative)}
-                      />
-                      {pfAlternative}
-                    </label>
-                  })
-                }
-                <br/>
-              </div>
-            })
-          }
+      
+      <div className="plantList">
+        <div className="plantList-center">
+          {loading ? <Loading/> : plants}
         </div>
-
         
-        <div className="pn">
-          {filteredPlants.map((plant) => {
-            return <div className="plants">
-                <img className="plant" src={plant.img}/>
-                <h3 className="name">{plant.name}</h3>
-            </div>
-          })}
-        </div>
       </div>
     );
+  }
+}
 
-    
+/*
+const plantFilters = {
+  "Functional Requirements": ["Rain Garden", "Pollinator Friendly", "Edible"],
+  Type: ["Flower", "Grass", "Buss & Berry"],
+  Origin: ["Native", "Norwegian Nursery"],
+  "Sun Light": ["Sun Seekers", "Shaddow lovers"],
+};
+
+function Plants() {
+  const [plantFilter, setPlantFilter] = React.useState({
+    "Functional Requirements": [],
+    Type: [],
+    Origin: [],
+    "Sun Light": [],
+  });
+
+  const [plants, setPlants] = React.useState([]);
+  const [filteredPlants, setFilteredPlants] = React.useState([]);
+
+  //Runs at load
+  React.useEffect(() => {
+    setPlants(plantList);
+    setFilteredPlants(plantList);
+  }, []);
+
+  //Runs when plantFilter is changed
+  React.useEffect(() => {
+    let newPlantList = [];
+    let tempPlants = plants;
+
+    tempPlants.map((plant) => {
+      let matchesFilter = false;
+
+      Object.keys(plantFilter).map((pf) => {
+        plantFilter[pf].map((pfAlternative) => {
+          if (Array.isArray(plant[pf]) && plant[pf].includes(pfAlternative))
+            matchesFilter = true;
+          else if (plant[pf] == pfAlternative) {
+            matchesFilter = true;
+          }
+        });
+      });
+
+      if (matchesFilter) {
+        newPlantList.push(plant);
+      }
+    });
+
+    if (newPlantList.length <= 0) newPlantList = plantList;
+
+    setFilteredPlants(newPlantList);
+  }, [plantFilter]);
+
+  //onClick handler for the filter checkboxes
+  const handleFilterChange = (event, pf, pfAlternative) => {
+    let currentFilter = plantFilter;
+
+    if (event.target.checked) {
+      currentFilter[pf].push(pfAlternative);
+    } else {
+      currentFilter[pf].splice(currentFilter[pf].indexOf(pfAlternative), 1);
     }
 
-ReactDOM.render(
-    <Plants />,
-    document.getElementById('root')
-);
+    setPlantFilter({ ...currentFilter });
+  };
 
-export default Plants;
+  return (
+    <div>
+      <div className="customize-content">
+        <div className="cust-concept-title">
+          <h1>PLANTER</h1>
+          <p>
+            I Oslo anbefales det med biologisk mangfoldige planter og
+            temperaturregulerende planter. Vi kan skrive mer her for å gi mer
+            informasjon.
+          </p>
+        </div>
+      </div>
+      <div className="filters">
+        {Object.keys(plantFilters).map((pf) => {
+          return (
+            <div>
+              <b className="pf">{pf}</b>
+              {plantFilters[pf].map((pfAlternative) => {
+                return (
+                  <label>
+                    <input
+                      type="checkbox"
+                      key={pfAlternative}
+                      value={pfAlternative}
+                      onClick={(event) =>
+                        handleFilterChange(event, pf, pfAlternative)
+                      }
+                    />
+                    {pfAlternative}
+                  </label>
+                );
+              })}
+              <br />
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="pn">
+        {filteredPlants.map((plant) => {
+          return (
+            <div className="plants">
+              <img className="plant" src={plant.img} />
+              <h3 className="name">{plant.name}</h3>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+ReactDOM.render(<Plants />, document.getElementById("root"));
+
+export default Plants;*/
