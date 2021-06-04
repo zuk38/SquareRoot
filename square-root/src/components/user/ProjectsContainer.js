f8bc7b6dddad041eb2ae37c94539c9d23fe20497
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-06-04 12:17:15 +0200

    Hot fix in project container

--- square-root/src/components/user/ProjectsContainer.js
+++ square-root/src/components/user/ProjectsContainer.js
@@ -1,20 +1,23 @@
 import React, { useContext, useEffect, useRef } from "react";
-import { Link } from "react-router-dom"; 
-import { CSSTransitionGroup  } from 'react-transition-group'; 
+import { Link } from "react-router-dom";
+import { CSSTransitionGroup } from "react-transition-group";
 import { withProjectConsumer } from "../../context/projects";
 
-var ReactCSSTransitionGroup = require('react-transition-group'); 
+var ReactCSSTransitionGroup = require("react-transition-group");
 
 function ProjectsContainer({ context }) {
   const { projects, loading } = context;
 
   if (!projects.length || !projects) {
-    return <h3>No Projects Created</h3>;
+    return (
+      <div className="p-table">
+        <h3>No Projects Created</h3>
+      </div>
+    );
   }
 
   return (
     <table className="p-table">
-      
       <tbody>
         <tr className="p-tr">
           <th className="p-th">PROSJEKTNAVN</th>
@@ -29,10 +32,8 @@ function ProjectsContainer({ context }) {
           transitionAppear={true}
           component="tr"
         >
-        
           {projects.map(
             ({ id, name, address, city, postalCode, createdAt }) => (
-              
               <tr key={id}>
                 <td className="p-td">{name}</td>
                 <td className="p-td">
@@ -40,10 +41,9 @@ function ProjectsContainer({ context }) {
                 </td>
                 <td className="p-td">{createdAt.split("T")[0]}</td>
               </tr>
-              
             )
           )}
-          </ReactCSSTransitionGroup>
+        </ReactCSSTransitionGroup>
       </tbody>
     </table>
   );
