7dbff65dd19d21e47af39d593c162b4b2e405cd4
gro97 <gro.oleivsgard@gmail.com>
2021-06-24 09:35:26 +0200

    cleaned up into folders

---
+++ square-root/src/pages/greenspaces/Rooftop.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import RooftopExample1 from '../greenspaces/RooftopExample1.js';
+
+ 
+class Rooftop extends Component {
+    render() {
+        return (
+            <div className="App">
+				<RooftopExample1></RooftopExample1>
+            </div>
+        );
+    }
+}
+ 
+export default Rooftop;
