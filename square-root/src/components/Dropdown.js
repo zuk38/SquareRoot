22946955df45f8019b9953c3b4a725a0f209f480
gro97 <gro.oleivsgard@gmail.com>
2021-07-02 11:29:20 +0200

    Fixed dropdown

--- square-root/src/components/Dropdown.js
+++ square-root/src/components/Dropdown.js
@@ -1,9 +1,9 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from "react";
 // import { Dropdown } from 'semantic-ui-react'
 // import 'semantic-ui-css/semantic.min.css'
 import "../styles/Plants.css";
 import "../styles/dropdown.css";
-import onClickOutside from 'react-onclickoutside';
+import useOutsideAlerter from "./hooks/useOutsideAlerter";
 import StyledComponents from '../styles/StyledComponents'
 
 
@@ -15,8 +15,10 @@ function Dropdown({
     const [open, setOpen] = useState(false);
     const [selection, setSelection] = useState([]);
     const toggle = () => setOpen(!open);
+    const closeDropDown = () => setOpen(false);
 
-    Dropdown.handleClickOutside = () => setOpen(false);
+    const dropdownRef = useRef(null);
+    useOutsideAlerter(dropdownRef, closeDropDown);
 
     function handleOnClick(item) {
         if (!selection.some(current => current.id === item.id)) {
@@ -43,60 +45,57 @@ function Dropdown({
     }
 
     return (
-      <div className="dd-container">
-        <div className="dd-wrapper">
-            <div tabIndex={0}
-                className="dd-header"
-                role="button"
-                onKeyPress={
-                    () => toggle(!open)
-                }
-                onClick={
-                    () => toggle(!open)
-            }
-            >
-                <div className="dd-header__title">
-                    <p className="dd-header__title--bold">
-                        {title}</p>
+        <div className="dd-container" ref={dropdownRef}>
+            <div className={selection[0] && open ? 'boldBox-open' : selection[0] ? 'boldBox-closed' : open ? 'boldBox-open' : 'dd-wrapper'}>
+                <div tabIndex={0}
+                    className="dd-header"
+                    
+                    onKeyPress={
+                        () => toggle(!open)
+                    }
+                    onClick={
+                        () => toggle(!open)
+                }>
+                    <div className="dd-header__title">
+                        <p className={selection[0] ? 'bold' : 'regular'}>
+                            {title}</p>
+                    </div>
+
                 </div>
-                
-            </div>
-            {
-            open && (
-                <ul className="dd-list">
-                  <div className="dd-borderBox">
-                <div className="dd-border"></div>
-                </div>
-                    {
-                    items.map(item => (
-                        <li className="dd-list-item"
-                            key={
-                                item.id
-                        }>
-                            <button type="button" className="dd-button"
-                                onClick={
-                                    () => handleOnClick(item)
-                            }>
-                                <span>{
-                                    item.value
-                                }</span>
-                                <span>
-                                    {
-                                    isItemInSelection(item) && <i class="fas fa-check"></i>
-                                }</span>
-                            </button>
-                        </li>
-                    ))
-                } </ul>
-            )
-        } </div>
+                {
+                open && (
+                    <ul className="dd-list" className={open ? 'dd-list-bold' : 'regularBox'}
+                        >
+                        <div>
+                            <div className="dd-borderBox">
+                                <div className="dd-border"></div>
+                            </div>
+                            {
+                            items.map(item => (
+                                <li className="dd-list-item"
+                                    key={
+                                        item.id
+                                }>
+                                    <button type="button" className="dd-button"
+                                        onClick={
+                                            () => handleOnClick(item)
+                                    }>
+                                        <span> {
+                                            isItemInSelection(item) && multiSelect ? <i class="far fa-check-square"></i> : multiSelect && <i class="far fa-square"></i>
+                                        }</span>
+                                        <span className={isItemInSelection(item) ? 'bold' : 'regular'}>{item.value}
+                                        </span>
+                                        
+                                    </button>
+                                </li>
+                            ))
+                        } </div>
+                    </ul>
+                )
+            } </div>
         </div>
     );
 }
 
-const clickOutsideConfig = {
-    handleClickOutside: () => Dropdown.handleClickOutside,
 
-};
-
-export default onClickOutside(Dropdown, clickOutsideConfig);
+export default Dropdown;
