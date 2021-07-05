40a573aa0f0430bfde9eff5842d11472fe8cb790
gro97 <gro.oleivsgard@gmail.com>
2021-07-05 15:06:03 +0200

    Editing why

--- square-root/src/pages/frontpages/Why.js
+++ square-root/src/pages/frontpages/Why.js
@@ -3,11 +3,29 @@ import "../../styles/Why.css";
 
 function Why() {
   return (
+
     <div>
+      <div className="why-hero">
+        <h1 className="why-txt--hero">
+          Hvorfor oss
+        </h1>
+        <p className="why-txt--hero-p">
+          Vi gjør det enklere for deg å velge grønt.
+        </p>
+      </div>
+
+      <div className="why-container">
+        <div className="why-img">
+          <img src="../images/oslo-flowers.jpeg" />
+        </div>
+      </div>
+
+{/*
+
       <div class="float-container">
         <div class="float-child">
           <div className="why">
-            <h1 className="why-name">Why Urban Green Spaces?</h1>
+            <h1 className="why-h1">HVORFOR SquareRoot?</h1>
             <p className="why-desc">
               Well designed urban green spaces are a powerful climate
               adaptation. They improve life quality, minimise risk of floods,
@@ -17,9 +35,10 @@ function Why() {
           </div>
         </div>
         <div class="float-child">
-          <img src="../images/plantbed.png" className="why-image" alt="?"></img>
+          <img src="../images/flowers-unsplash.jpeg" className="why-image" alt="?"></img>
         </div>
       </div>
+*/}
 
       <div className="why-reasons">
         <h2 className="five-reasons">5 good reasons to choose green spaces</h2>
