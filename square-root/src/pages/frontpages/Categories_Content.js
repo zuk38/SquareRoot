8e0790fca4fe95e302255c112a8ae5f14580b0a3
gro97 <gro.oleivsgard@gmail.com>
2021-07-21 15:16:29 +0200

    Adding content for category info

--- square-root/src/pages/frontpages/Categories_Content.js
+++ square-root/src/pages/frontpages/Categories_Content.js
@@ -2,43 +2,42 @@ import React from 'react';
 import "../../styles/frontpages.css";
 
 
-
 export default function Categories_Content() {
 
     const insert_values = [
         {
-            name:"rooftop",
-            img: "../images/portrait--rooftop.jpg",
+            name: "rooftop",
+            hero_img: "../images/landscape--rooftop.jpg",
+            hero_img_overlay: "Netflix HQ, Amsterdam",
             header: "Takterrasse",
             subheader: "Takterrasser is simply dummy text of the printing and typesetting industry.",
             descr: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
         },
-        {   
-            name:"biodiversity",
+        {
+            name: "biodiversity",
             img: "../images/portrait--outdoor-bio.jpg",
             header: "Biomangfold",
             subheader: "Biomangfold is simply dummy text of the printing and typesetting industry.",
             descr: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-        
+
         },
         {
-            name:"rainbed",
+            name: "rainbed",
             img: "../images/rainbed.jpg",
             header: "Regnseng",
             subheader: "Regnsenger is simply dummy text of the printing and typesetting industry.",
             descr: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-        
+
         },
         {
-            name:"walls",
+            name: "walls",
             img: "../images/portrait--indoor-wall.jpg",
             header: "Grønne vegger",
             subheader: "Grønne vegger is simply dummy text of the printing and typesetting industry.",
             descr: "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
-        
-        },
-        {
-            name:"indoor",
+
+        }, {
+            name: "indoor",
             img: "../images/portrait--indoor-green.jpg",
             header: "Innendørsplanter",
             subheader: "Innendørsplanter is simply dummy text of the printing and typesetting industry.",
@@ -48,26 +47,30 @@ export default function Categories_Content() {
 
     return (
         <div>
-            <div className="c-section background-grey">
-                
-              
-                   <div className="o-container">
-                    <h2 className="c-txt--sectiontitle c-mrg--bottom">{insert_values[0].header}</h2>
-                    <h3 className="c-txt--sectiontitle c-mrg--bottom">{insert_values[0].subheader}</h3>
 
-                    <div className="c-grid--1x">
-                    <ul className="no-scroll-grid">
-                               <img src={insert_values[0].img} />
-        
-                                <p>{insert_values[0].descr}</p>
-                         
-                            </ul>
-                        </div>
-                        </div>
-                    
+            <div className="c-hero">
+                <h1 className="c-txt--hero">
+                    {
+                    insert_values[0].header
+                } </h1>
+                <p className="c-txt--hero-p">
+                    {
+                    insert_values[0].subheader
+                }</p>
             </div>
 
+            <div className="c-container white-on-hover">
+                <img className="c-img" src={
+                        insert_values[0].hero_img
+                    }/>
+                    <div className="img-overlay">
+                        <div className="text-overlay">{
+                        insert_values[0].hero_img_overlay
+                    }</div></div>
+            </div>
+          
             
+
 
         </div>
 
