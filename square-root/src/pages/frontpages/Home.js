080d6a6af16594cecec005b86a1413d69d5ad6e2
gro97 <gro.oleivsgard@gmail.com>
2021-07-28 12:07:29 +0200

    Stuff for homepage

--- square-root/src/pages/frontpages/Home.js
+++ square-root/src/pages/frontpages/Home.js
@@ -19,36 +19,91 @@ export default function Home(props) {
                     <p>Lorem ipsum lorem ipsum lorem upsum</p>
                 </section>
 
-                <section id="second">
-                    <div className="flex-container">
-                         <h1>Takterrasse</h1>
-                         <button className="action-btn zoom-on-hover">Les mer<i class="fas fa-angle-double-right"></i></button>
-                 </div>
-                </section>
+        
 
+                <section id="second">
+                    <div className="c-grid--2x2 padding-sm bottom-container">
+                        
+                        <h1 className="item1--2x2">Takterrasse</h1>
+                        <div className="item2--2x2 cat-descr">
+                            Sett sammen en takehage med forhåndsutvalgte planter<br/>
+                            Spar tid og penger på å finne plantene som passer i området <br/>
+                            
+                        
+                        </div>
+                        <button className="item3--2x2 action-btn zoom-on-hover">Les mer<i class="fas fa-angle-double-right"></i>
+                        </button>
+
+
+                    </div>
+
+                </section>
 
                 <section id="third">
-                    <div className="flex-container">
-                         <h1>Regnseng</h1>
-                         <button className="action-btn zoom-on-hover">Les mer<i class="fas fa-angle-double-right"></i></button>
-                 </div>
+                    <div className="c-grid--3x2 padding-sm top-container">
+                        <h1 className="item1--2x2">Regnseng</h1>
+                        <button className="item3--2x2 action-btn zoom-on-hover float-left">Les mer<i class="fas fa-angle-double-right"></i>
+                        </button>
+                        <div className="item2--2x2 cat-descr">
+                            Overvannshåndtering
+                            <br/>
+                            Filtrerer vannet<br/>
+                            Beskytter dammer og kystlinjer<br/>
+                            Øker bestanden av gunstige insekter som eliminerer skadedyr
+                            <br/>
+                            Reduserer sjansen for flom i bygningen
+                            <br/>
+                            Øker verdien av eiendommen
+                        </div>
+                    </div>
+
                 </section>
+
 
                 <section id="fourth">
-                    <div className="flex-container">
-                         <h1>Biomangfold</h1>
-                         <button className="action-btn zoom-on-hover">Les mer<i class="fas fa-angle-double-right"></i></button>
-                 </div>
-                </section>
+                    <div className="c-grid--2x2 padding-sm bottom-container">
+                        <div className="item1--2x2 cat-descr">
+                            Overvannshåndtering
+                            <br/>
+                            Filtrerer vannet<br/>
+                            Beskytter dammer og kystlinjer<br/>
+                            Øker bestanden av gunstige insekter som eliminerer skadedyr
+                            <br/>
+                            Reduserer sjansen for flom i bygningen
+                            <br/>
+                            Øker verdien av eiendommen
+                        </div>
+                        <h1 className="item2--2x2">Biomangfold</h1>
+                        <button className="item3--2x2 action-btn zoom-on-hover">Les mer<i class="fas fa-angle-double-right"></i>
+                        </button>
 
+
+                    </div>
+
+                </section>
+           
                 <section id="fifth">
-                    <div className="flex-container">
-                         <h1>Innendørs</h1>
-                         <button className="action-btn zoom-on-hover">Les mer<i class="fas fa-angle-double-right"></i></button>
-                 </div>
+                    <div className="c-grid--2x2 padding-sm bottom-container">
+                        <div className="item1--2x2 cat-descr">
+                            Overvannshåndtering
+                            <br/>
+                            Filtrerer vannet<br/>
+                            Beskytter dammer og kystlinjer<br/>
+                            Øker bestanden av gunstige insekter som eliminerer skadedyr
+                            <br/>
+                            Reduserer sjansen for flom i bygningen
+                            <br/>
+                            Øker verdien av eiendommen
+                        </div>
+                        <h1 className="item2--2x2">Innendørs</h1>
+                        <button className="item3--2x2 action-btn zoom-on-hover">Les mer<i class="fas fa-angle-double-right"></i>
+                        </button>
+
+
+                    </div>
+
                 </section>
 
-      
 
             </div>
         </div>
