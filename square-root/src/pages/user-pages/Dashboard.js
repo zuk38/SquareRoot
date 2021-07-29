c9ab5cfebbe23a1ead5da59c597dab46d499316d
gro97 <gro.oleivsgard@gmail.com>
2021-07-29 11:16:18 +0200

    Included button add greenspace

--- square-root/src/pages/user-pages/Dashboard.js
+++ square-root/src/pages/user-pages/Dashboard.js
@@ -143,7 +143,8 @@ function Dashboard(props) {
           </div>
           <br />
           <h2 className="p-h2">Rolle: {currentMember.role}</h2>
-
+          </div>
+  </div>
           {/* --- REMOVING "ADD GREENSPACE" BUTTON ---
 B/c: The user should understand that in order to add greenspace/plant, 
 they should navigate through the menu, rather than us setting up another route for them to take.
@@ -151,8 +152,7 @@ they should navigate through the menu, rather than us setting up another route f
 <button className="btn-new-greenspace">
           <i class="fas fa-plus"></i>Legg til grøntområde
       </button>
-</div>
-  </div>
+
           {renderSwitch(currentPage)}
         </div>
   );
