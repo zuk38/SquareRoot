26c0ddb11e3065bcf9322aca4fb974b7c40ca148
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-08-12 20:56:06 +0400

    oops

---
+++ square-root/src/pages/frontpages/HomeData.js
@@ -0,0 +1,129 @@
+export const homeData = [
+  {
+    id: "takterrasse",
+    headline: "Takterrasse",
+    img: "../images/landscape--rooftop_redigert.png",
+    descriptionItems: [
+      {
+        text: "Sett sammen en takhage med forhåndsutvalgte planter",
+      },
+      {
+        text: "Spar tid og penger på å finne plantene som passer i området",
+      },
+      {
+        text:
+          "Vi utvikler skreddersydde tilbud for riktige planter til riktig sted og konsepter som takhager for å øke biologisk mangfold og regnsenger for å håndtere kraftig nedbør og planter.",
+      },
+    ],
+    path: "/categories/takterrasse",
+    nextId: "regnseng",
+    nextName: "Regnseng",
+  },
+  {
+    id: "regnseng",
+    headline: "Regnseng",
+    img: "../images/landscape--rainbed_redigert.jpg",
+    descriptionItems: [
+      {
+        text: "Overvannshåndtering",
+      },
+      {
+        text: "Beskytter dammer og kystlinjer",
+      },
+      {
+        text: "Øker bestanden av gunstige insekter",
+      },
+      {
+        text: "Reduserer sjansen for flom i bygningen",
+      },
+      {
+        text: "Øker verdien av eiendommen",
+      },
+    ],
+    path: "/categories/regnseng",
+    nextId: "biomangfold",
+    nextName: "Biomangfold",
+  },
+  {
+    id: "biomangfold",
+    headline: "Biomangfold",
+    img: "../images/landscape--biodiversity2.jpg",
+    descriptionItems: [
+      {
+        text: "Overvannshåndtering",
+      },
+      {
+        text: "Filtrerer vannet",
+      },
+      {
+        text: "Beskytter dammer og kystlinjer",
+      },
+      {
+        text: "Øker bestanden av gunstige insekter som eliminerer skadedyr",
+      },
+      {
+        text: "Reduserer sjansen for flom i bygningen",
+      },
+      {
+        text: "Øker verdien av eiendommen",
+      },
+    ],
+    path: "/categories/biomangfold",
+    nextId: "innendørsplanter",
+    nextName: "Innendørsplanter",
+  },
+  {
+    id: "innendørsplanter",
+    headline: "Innendørsplanter",
+    img: "../images/landscape--indoor3.jpeg",
+    descriptionItems: [
+      {
+        text: "Overvannshåndtering",
+      },
+      {
+        text: "Filtrerer vannet",
+      },
+      {
+        text: "Beskytter dammer og kystlinjer",
+      },
+      {
+        text: "Øker bestanden av gunstige insekter som eliminerer skadedyr",
+      },
+      {
+        text: "Reduserer sjansen for flom i bygningen",
+      },
+      {
+        text: "Øker verdien av eiendommen",
+      },
+    ],
+    path: "/categories/innendørsplanter",
+    nextId: "grønneVegger",
+    nextName: "Grønne vegger",
+  },
+  {
+    id: "grønneVegger",
+    headline: "Grønne Vegger",
+    img: "../images/landscape--indoor_redigert.jpg",
+    descriptionItems: [
+      {
+        text: "Overvannshåndtering",
+      },
+      {
+        text: "Filtrerer vannet",
+      },
+      {
+        text: "Beskytter dammer og kystlinjer",
+      },
+      {
+        text: "Øker bestanden av gunstige insekter som eliminerer skadedyr",
+      },
+      {
+        text: "Reduserer sjansen for flom i bygningen",
+      },
+      {
+        text: "Øker verdien av eiendommen",
+      },
+    ],
+    path: "/categories/grønne%20vegger",
+  },
+];
