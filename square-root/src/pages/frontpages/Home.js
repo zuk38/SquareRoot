4d3c19de7c33c81acbf4370b956974caa29c1d9d
gro97 <gro.oleivsgard@gmail.com>
2021-08-23 23:29:57 +0200

    Added hover images and removed text

--- square-root/src/pages/frontpages/Home.js
+++ square-root/src/pages/frontpages/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, {useEffect, useState, useRef} from "react";
 import Hero from "../../components/Hero";
 import Banner from "../../components/Banner";
 import PartnerContainer from "../../components/PartnerContainer";
@@ -9,17 +9,19 @@ import "../../styles/Home.css";
 import logo from "../../images/logo-white-green.png";
 import arrowDown from "../../icons/down-arrow.svg";
 import arrowDownWhite from "../../icons/down-arrow-white.png";
-import { useHistory } from "react-router-dom";
-import { homeData } from "./HomeData";
+import {useHistory} from "react-router-dom";
+import {homeData} from "./HomeData";
 
 export default function Home(props) {
     const executeScroll = (myRef) => {
         let element;
-        if (!myRef) element = document.getElementById("first")
-        else element = document.getElementById(myRef)
-        element && element.scrollIntoView({block: "start", inline: "nearest", behavior: "smooth"});
-      };
-      
+        if (!myRef) 
+            element = document.getElementById("first")
+         else 
+            element = document.getElementById(myRef)
+         element && element.scrollIntoView({block: "start", inline: "nearest", behavior: "smooth"});
+    };
+
     return (
         <div className="homepage">
             <div className="front-container disable-scrollbars">
@@ -32,8 +34,7 @@ export default function Home(props) {
                                 className="logo-large"></img>
 
                         </div>
-                    
-            
+
 
                         <div className="grid-item1--hero">
 
@@ -47,17 +48,17 @@ export default function Home(props) {
                                 <button className="front-btn u-mrg--txt3">Gå til Beta</button>
                             </div>
                         </div>
-               
 
 
                         <div className="grid-item2--hero">
                             <div className="hero-f-punchline">
                                 <span className="list-item">
-                                <h1> <img src="../images/logo-asterisk-white.png" className="list-item-icon"/>
+                                    <h1>
+                                        <img src="../images/logo-asterisk-white.png" className="list-item-icon"/>
 
-                                    
+
                                         Planlegg, design og bestill grøntområder inne, ute og på toppen av
-                                                                                                                                                                                                                                                                                  bygninger
+                                                                                                                                                                                                                                                                                                                                                                  bygninger
                                         <br/>-
                                         <i className="ibm-italic">alt på ett sted</i>.
                                     </h1>
@@ -80,97 +81,118 @@ export default function Home(props) {
                 </section>
 
                 <section id="second">
-                    <div className="c-grid--2x1-equal padding-sm ">
-                        <div className="grid-item2--equal top-container">
-                            <h1>WHY</h1>
-                            <div className="cat-descr">
-                            Sett sammen en takhage med forhåndsutvalgte planter
-                            <br/>
-                            Spar tid og penger på å finne plantene som passer i området
-                            <br/>
-                            Vi utvikler skreddersydde tilbud for riktige planter til riktig
-                             <br/>
-                        
-                        </div>
-                        </div>
+                    <div className="s-container">
+                        <div className="s-center">
+                            <div className="c-grid--2x1-equal padding-sm ">
+                                <div className="top-container">
+                                    <div className="float-right">
+                                        <h1>WHY</h1>
+                                    
 
-                        <img className="grid-item1--equal" src="../images/landscape--rooftop_redigert.png"></img>
-                       {/*  
+                                    <div className="cat-descr">
+                                        Sett sammen en takhage med forhåndsutvalgte planter
+                                        <br/>
+                                        Spar tid og penger på å finne plantene som passer i området
+                                        <br/>
+                                        Vi utvikler skreddersydde tilbud for riktige planter til riktig
+                                        <br/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="float-left">
+                                <img src="../images/landscape--rooftop_redigert.png"></img>
+                                </div>
+                                {/*  
                         <button className="btn-scroll-down zoom-on-hover" href="#third">
                             <i class="fas fa-angle-double-down"></i>Regnseng
                         </button>
-                        */}   
-                    </div>
+                        */} </div>
+                        </div>
 
-                    <div className="c-section quote">
-                        new info
+                        <div className="c-section quote">
+                            new info
+                        </div>
                     </div>
-
                 </section>
 
                 <section id="third">
-                <div className="c-grid--2x1-equal padding-sm">
-                        <div className="grid-item1--equal top-container">
-                            <h1>WHAT and HOW</h1>
-                            <div className="cat-descr">
-                            Sett sammen en takhage med forhåndsutvalgte planter
-                            <br/>
-                            Spar tid og penger på å finne plantene som passer i området
-                            <br/>
-                            Vi utvikler skreddersydde tilbud for riktige planter til riktig
-                             <br/>
-                        
-                        </div>
-                        </div>
+                    <div className="s-container">
+                        <div className="s-center">
+                            <div className="c-grid--2x1-equal padding-sm">
+                                <div className="top-container">
+                                    <div>
+                                        <h1>WHAT and HOW</h1>
+                                    </div>
 
-                        <img className="grid-item2--equal" src="../images/landscape--rooftop_redigert.png"></img>
-                       {/*  
+                                    <div className="cat-descr">
+                                        Sett sammen en takhage med forhåndsutvalgte planter
+                                        <br/>
+                                        Spar tid og penger på å finne plantene som passer i området
+                                        <br/>
+                                        Vi utvikler skreddersydde tilbud for riktige planter til riktig
+                                        <br/>
+
+                                    </div>
+                                </div>
+
+                                <img className="grid-item2--equal" src="../images/landscape--rooftop_redigert.png"></img>
+                                {/*  
                         <button className="btn-scroll-down zoom-on-hover" href="#third">
                             <i class="fas fa-angle-double-down"></i>Regnseng
                         </button>
-                        */}   
-                    </div>
-
-                    <div className="c-section u-txt--center quote c-grid--3x3fixed">
-                        <div>
-                            <img src="logo" />
-                            <h1>Takhage</h1>
-                            <p>description</p>
+                        */} </div>
                         </div>
-                        <div>
-                        <img src="logo" />
-                            <h1>Regnbed</h1>
-                            <p>description</p>
-                        </div>
-                        <div>
-                        <img src="logo" />
-                            <h1>Innendørs</h1>
-                            <p>description</p>
-                        </div>
-                    
                     </div>
                 </section>
 
                 <section id="fourth">
-               <div className="padding-sm">
-                        <div className="top-container">
-                            <h1>I samarbeid med</h1>
+                    <div className="s-container">
+                        <div className="s-center">
+                            <div className="c-section u-txt--center quote c-grid--3x3fixed">
+                                <div>
+                                    <img src="logo"/>
+                                    <h1>Takhage</h1>
+                                    <p>description</p>
+                                </div>
+                                <div>
+                                    <img src="logo"/>
+                                    <h1>Regnbed</h1>
+                                    <p>description</p>
+                                </div>
+                                <div>
+                                    <img src="logo"/>
+                                    <h1>Innendørs</h1>
+                                    <p>description</p>
+                                </div>
+                            </div>
                         </div>
-                        
-                        <div className="c-section c-grid--span">
-                            <img src="../images/nibio_logo.jpeg" />
-                            <img src="../images/edit_logo.jpeg" />
-                            <img src="../images/teknologi-formidling_logo.png" />
-                            
-                            <img src={logo} />
-                            <img src={logo} />
-                            <img src={logo} />
-                            <img src={logo} />
-                        </div>
-                        </div>
-                        </section>
+                    </div>
+                </section>
 
+                <section id="fifth">
+                    <div className="s-container">
+                        <div className="s-center">
+                            <div className="padding-sm">
+                                <div className="top-container">
+                                    <h1>I samarbeid med</h1>
+                                </div>
+
+                                <div className="c-section c-grid--span">
+                                    <img src="../images/nibio_logo.jpeg"/>
+                                    <img src="../images/edit_logo.jpeg"/>
+                                    <img src="../images/teknologi-formidling_logo.png"/>
+
+                                    <img src={logo}/>
+                                    <img src={logo}/>
+                                    <img src={logo}/>
+                                    <img src={logo}/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+            </div>
         </div>
-    </div>
-  );
+    );
 }
