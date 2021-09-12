549194433c781b07cd0004dddcff897ec702ad5c
gro97 <gro.oleivsgard@gmail.com>
2021-09-12 17:11:29 +0200

    Adjusted design for Partner and added contactform

--- square-root/src/pages/frontpages/Partner.js
+++ square-root/src/pages/frontpages/Partner.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { API } from "aws-amplify";
+import React, {useState} from "react";
+import {API} from "aws-amplify";
 import Loading from "../../components/Loading";
 import useForm from "../../components/hooks/useForm";
 import validate from "../../components/utility/ContactFormValidation";
@@ -7,56 +7,92 @@ import "../../styles/Partner.css";
 import PartnerContainer from "../../components/PartnerContainer";
 
 export default function Partner(props) {
-  const { values, errors, handleChange, handleSubmit } = useForm(
-    addContact,
-    validate,
-    callback
-  );
+    const {values, errors, handleChange, handleSubmit} = useForm(addContact, validate, callback);
 
-  const [isSending, setIsSending] = useState(false);
-  async function addContact() {
-    setIsSending(true);
-    // create request body
-    const data = {
-      body: {
-        name: values.name,
-        email: values.email,
-        message: values.message,
-      },
-    };
-    console.log(data);
-    // call the API gateway
-    const apiData = await API.post("contactFormApi", "/contact", data);
-    console.log({ apiData });
-    setIsSending(false);
-    alert("Mail sent!");
-  }
+    const [isSending, setIsSending] = useState(false);
+    async function addContact() {
+        setIsSending(true);
+        // create request body
+        const data = {
+            body: {
+                name: values.name,
+                email: values.email,
+                message: values.message
+            }
+        };
+        console.log(data);
+        // call the API gateway
+        const apiData = await API.post("contactFormApi", "/contact", data);
+        console.log({apiData});
+        setIsSending(false);
+        alert("Mail sent!");
+    }
 
-  async function callback() {
-    console.log(values);
-  }
+    async function callback() {
+        console.log(values);
+    }
 
-  return (
-    <>
-      {" "}
-      {isSending ? (
-        <Loading />
-      ) : (
-        <div className="o-container">
-          <div className="c-hero">
-            <h1 className="c-txt--hero">Interessert i SquareRoot?</h1>
-            <div className="c-txt--hero-p">
-              Vi ønsker å samarbeide med alle aktører i planleggingen og
-              utformingen av urbane grøntområder.
-              <br />
-              <br />
-              Dette er for at vi kan gjøre arbeidsprosessen sømløs slik at du
-              kan lage og vedlikeholde motstandsdyktige grøntområder.
-            </div>
-          </div>
-          <PartnerContainer />
-        </div>
-      )}{" "}
-    </>
-  );
+    return (
+        <> {" "}
+            {
+            isSending ? (
+                <Loading/>) : (
+                <div className="o-container">
+                    <div className="c-hero">
+                        <h1 className="c-txt--hero">Bli partner</h1>
+
+                        <div className="c-mrg--bottom"></div>
+
+                        <div className="c-grid--2x2">
+                            <div className="grid-item3 margin-left">
+                                <img src="../images/partner.jpg" className="c-img flip-img"/>
+                            </div>
+
+
+                            <div className="grid-item1">
+
+                                <div className="c-txt-punchline--sm">
+                                    <h2 className="c-txt-punchline--sm">
+                                        Sammen gjør vi byene våres grønne. 
+                                                      
+                                                            Bokstavelig talt.
+                                    </h2>
+                                    Vi ønsker å samarbeide med alle aktører i planleggingen og
+                                                                                      utformingen av urbane grøntområder. Dette er for at vi kan gjøre arbeidsprosessen sømløs slik at du
+                                                                                      kan lage og vedlikeholde motstandsdyktige grøntområder.
+
+                                    <br/>
+                                    <br/>
+                                    Vi ønsker å samarbeide med alle aktører i planleggingen og
+                                                                                      utformingen av urbane grøntområder. Dette er for at vi kan gjøre arbeidsprosessen sømløs slik at du
+                                                                                      kan lage og vedlikeholde motstandsdyktige grøntområder.
+
+
+                                </div>
+                            </div>
+
+
+                            <div className="c-mrg--bottom"></div>
+                        </div>
+                        <div className="c-section c-section--cta background-grey">
+                            <div className="o-container">
+                                <h2 className="c-txt-sectiontitle--sm u-txt--center">
+                                    Ta kontakt for en uforpliktende samtale
+                                </h2>
+                                <div className="u-txt--center">
+                                <PartnerContainer/>
+                                </div>
+                                
+
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+
+            )
+        }
+            {" "} </>
+    );
 }
