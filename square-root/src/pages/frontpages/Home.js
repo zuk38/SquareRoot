8a3cd1265adc1996a1fa040d2c09859b76bec3c9
gro97 <gro.oleivsgard@gmail.com>
2021-09-19 11:33:35 +0200

    Edited homepage

--- square-root/src/pages/frontpages/Home.js
+++ square-root/src/pages/frontpages/Home.js
@@ -74,10 +74,10 @@ export default function Home(props) {
                     <div className="c-grid--3x1">
                         <div className="grid-item1">
 
-                            <img className="column-img" src="../images/eufemia.jpg"></img>
+                            <img className="column-img" src="../images/hands-rooftop.png"></img>
                         </div>
                         <div className="grid-item2">
-                            <img className="column-img" src="../images/eufemia.jpg"></img>
+                            <img className="column-img" src="../images/r-falcatum.jpg"></img>
 
                         </div>
 
@@ -99,8 +99,8 @@ export default function Home(props) {
                             <div className="main-punchline">
                                 <div className="main-punchline--p">
                                     At SquareRoot, we believe sustainable cities are
-                                    <em>
-                                        our future</em>. With lots of green spaces inside, outside and top of buildings.
+                                    <em>&nbsp;
+                                                                                                                        our future</em>. With lots of green spaces inside, outside and top of buildings.
                                 </div>
                             </div>
 
@@ -111,7 +111,6 @@ export default function Home(props) {
 
                 </div>
             </section>
-
 
             <section id="third">
 
@@ -136,7 +135,7 @@ export default function Home(props) {
                             <div className="main-punchline">
                                 <div className="main-punchline--p">
                                     Together with NIBIO, biologists, landscape architects and plant nurseries we 
-                                                                                                                                                        have developed plant lists for urban greening and standard concepts, such as roof gardens and rain beds, to manage heavy rainfall and to increase biodiversity.
+                                                                                                                                                                                                                                                                                                                                                                                                                    have developed plant lists for urban greening and standard concepts, such as roof gardens and rain beds, to manage heavy rainfall and to increase biodiversity.
 
                                 </div>
                             </div>
@@ -153,6 +152,75 @@ export default function Home(props) {
 
             </section>
 
+            <section id="fifth">
+                <div className="o-container">
+                    <div className="c-grid--3x1">
+
+                        <div className="grid-item1">
+                            <div className="img-wrap-rooftop">
+                                <div className="u-txt--middle">
+                                    <em>Rooftop</em>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="grid-item2">
+                            <div className="main-punchline--p">
+                                <div className="short-border"></div>
+
+                                <p className="c-section--p">Optimise your roof surface with one of our
+                                    <em>&nbsp;rooftop&nbsp;</em>
+                                    concepts. Our plants and hardware are chosen and designed to meet your specific needs.
+                                </p>
+
+
+                            </div>
+                        </div>
+
+
+                        <div className="grid-item3">
+                            <div className="img-wrap-rainbed">
+                                <div className="u-txt--middle">
+                                    <em>Rainbed</em>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="grid-item4">
+                            <div className="main-punchline--p">
+                                <div className="short-border"></div>
+                                <p>
+                                    Our plants for
+                                    <em>&nbsp;rainbeds&nbsp;</em>
+                                    are carefully chosen with plants for the wet-zone, able to stand up to 48 hours in the water and drought tolerant plants at the borders, the dry-zone.
+                                    <br/>
+                                </p>
+                            </div>
+                        </div>
+
+                        <div className="grid-item9">
+                            <div className="img-wrap-indoor">
+                                <div className="u-txt--middle">
+                                    <em>Indoor</em>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="grid-item10">
+                            <div className="main-punchline--p">
+                                <div className="short-border"></div>
+
+                                <p>Our partner Leaf has extensive expertise with
+                                    <em>&nbsp;indoor&nbsp;</em>
+                                    plants. Incredible aesthetics are combined with minimal maintenance.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </section>
+
             <section id="fourth">
                 <div className="o-container">
                     <div className="c-grid--3x1">
@@ -161,112 +229,39 @@ export default function Home(props) {
                             <div className="main-punchline is-white">
                                 <div className="main-punchline--lrg">
                                     <em>COMING SOON</em>
-                                    <br />
-                                    <br />
+                                    <br/>
+                                    <br/>
                                 </div>
-                            
+
                             </div>
                             <div className="short-border-white"></div>
-                            
-                            <div className="main-punchline">
-                            <div className="main-punchline--p is-white">
-                            Our digital platform makes finding and sourcing plants
-                                        <em>
-                                            easy and available everywhere</em>.
-                                            <br />
-                                            <br />
-                                            Through our network of local plant nurseries, we help you find the 
-                                                                                                                                                                                                                                                  right plants for the right spot and show what is available to have it delivered in time.
-                                        <br/>
-                                        <br/>
-                                    
 
-                                      </div>
+                            <div className="main-punchline">
+                                <div className="main-punchline--p is-white">
+                                    Our digital platform makes finding and sourcing plants
+                                    <em>&nbsp;
+                                                                                                                        easy and available everywhere</em>.
+                                    <br/>
+                                    <br/>
+                                    Through our network of local plant nurseries, we help you find the 
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              right plants for the right spot and show what is available to have it delivered in time.
+                                    <br/>
+                                    <br/>
+
+
+                                </div>
                             </div>
                         </div>
 
                         <div className="grid-item3-vertical">
-                            <RequestDemo />
+                            <RequestDemo/>
                         </div>
 
-                    </div>
-                    {/* 
-                    <div className="c-hero">
-                        <div className="c-grid--2x2">
-
-                            <div className="grid-item3 u-txt--center">
-                                <img className="c-img" src="../images/platform-shot.png"></img>
-
-                            </div>
-
-                            <div className="grid-item1">
-
-                                <div className="top-container">
-
-
-                                    <div className="main-punchline--lrg">
-                                        Our digital platform makes finding and sourcing plants
-                                        <em>
-                                            easy and available everywhere</em>.
-                                    </div><br/>
-
-
-                                    <div className="main-punchline">
-                                        Through our network of local plant nurseries, we help you find the 
-                                                                                                                                                                                                                                                  right plants for the right spot and show what is available to have it delivered in time.
-                                        <br/>
-                                        <br/>
-                                        <button className="btn-call2action zoom-on-hover">REQUEST DEMO</button>
-
-
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    */} </div>
-
-            </section>
-
-
-            <section id="fifth">
-                <div className="grid-container">
-                    <div className="c-hero">
-                        <div className="c-section c-grid--3x3fixed">
-                            <div className="c-section--container">
-                                <h1>Roof garden</h1>
-
-
-                                <img src="../images/landscape--rooftop_redigert.png"/>
-
-                                <p className="c-section--p">Optimise your roof surface with one our concepts. Our plants and hardware are chosen and designed to meet your specific needs.
-                                </p>
-
-                            </div>
-                            <div>
-
-                                <h1>Rainbed</h1>
-
-                                <img src="../images/leaf_rooftop_4_redigert.jpg"/>
-
-                                <p>
-                                    Our plants for this urban green space are carefully chosen with plants for the wet-zone, able to stand up to 48 hours in the water and drought tolerant plants at the borders, the dry-zone.
-                                    <br/>
-                                </p>
-                            </div>
-
-                            <div>
-                                <h1>Indoor</h1>
-
-                                <img src="../images/landscape--indoor_redigert.jpg"/>
-
-                                <p>Our partner Leaf has extensive expertise with indoor plants. Incredible aesthetics are combined with minimal maintenance.
-                                </p>
-                            </div>
-                        </div>
                     </div>
                 </div>
+
             </section>
+
 
             <section id="sixth">
                 <div className="c-section c-section--cta quote">
@@ -275,16 +270,16 @@ export default function Home(props) {
 
 
                             By integrating nature in your project, it will help
-                                                                                                                    meet the requirements
-                                                                                                                        for stormwater management.
-                                                                                                                        Using local plants increases
-                                                                                                                       biodiversity
-                                                                                                                        and thus BREEAM points.
+                                                                                                                                                                                                                                                                                                                        meet the requirements
+                                                                                                                                                                                                                                                                                                                            for stormwater management.
+                                                                                                                                                                                                                                                                                                                            Using local plants increases
+                                                                                                                                                                                                                                                                                                                           biodiversity
+                                                                                                                                                                                                                                                                                                                            and thus BREEAM points.
 
                             <br/>
                             <br/>
                             Building projects with a high environmental footprint increases the value of your property and
-                                                                                        keeps our cities healthy.
+                                                                                                                                                                                                                                                                                            keeps our cities healthy.
                             <br/>
 
                         </div>
@@ -295,23 +290,18 @@ export default function Home(props) {
             </section>
 
             <section id="seventh">
-                <div className="s-container">
-                    <div className="s-center">
-                        <div className="padding-sm">
-                            <div className="top-container">
-                                <h1>I samarbeid med</h1>
-
-
-                                <div className="c-section c-grid--span">
-                                    <img src="../images/nibio_logo.jpeg"/>
-                                    <img src="../images/edit_logo.jpeg"/>
-                                    <img src="../images/teknologi-formidling_logo.png"/>
-                                    <img src="../images/nibio_logo.jpeg"/>
-                                    <img src="../images/edit_logo.jpeg"/>
-                                    <img src="../images/teknologi-formidling_logo.png"/>
-                                </div>
-                            </div>
-                        </div>
+                <div className="o-container">
+                <div className="main-punchline--lrg">
+                    I samarbeid med
+                </div>
+                    <div className="c-grid--fill">
+                        <div><img src="../images/nibio_logo.jpeg"/></div>
+                        <div><img src="../images/edit_logo.jpeg"/></div>
+                        <div><img src="../images/teknologi-formidling_logo.png"/></div>
+                        <div> <img src="../images/innovasjon-norge-big.png"/></div>
+                        <div> <img src="../images/tekna-logo.jpeg"/></div>
+                        <div><img src="../images/sin-logo.png"/></div>
+                        <div><img src="../images/gg-logo.png"/></div>
                     </div>
                 </div>
             </section>
