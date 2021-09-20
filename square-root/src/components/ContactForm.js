43b7790ef10678b784d3e75e24f53a68686ed0e1
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-09-20 13:27:32 +0200

    Contact form finished

--- square-root/src/components/ContactForm.js
+++ square-root/src/components/ContactForm.js
@@ -1,110 +1,195 @@
-import { useTranslation } from "react-i18next";
+import React, { useState } from "react";
+import { API } from "aws-amplify";
+import Loading from "./Loading";
+import useForm from "./hooks/useForm";
+import validate from "./utility/ContactFormValidation";
+import "../styles/Partner.css";
+import { useHistory } from "react-router-dom";
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
 
-export default function ContactForm() {
-  const { t } = useTranslation();
+export default function ContactForm(props) {
+  const history = useHistory();
+  const pathname = history.location.pathname;
+
+  const { values, errors, handleChange, handleSubmit } = useForm(
+    addContact,
+    validate,
+    callback
+  );
+
+  const [isSending, setIsSending] = useState(false);
+  const [sent, setSent] = useState(false);
+
+  async function addContact() {
+    setIsSending(true);
+    // create request body
+    const data = {
+      body: {
+        name: values.name,
+        email: values.email,
+        message: values.message,
+        phone: values.phone || "",
+        location: values.location || "",
+      },
+    };
+    // call the API gateway
+    const apiData = await API.post("contactFormApi", "/contact", data);
+    console.log({ apiData });
+    setIsSending(false);
+  }
+
+  function callback() {
+    setSent(true);
+  }
+
   return (
     <>
-      <form>
-        <div className="o-form__group">
-          <h3 className="o-form__heading o-form__heading">
-            {t("contact_page.send")}
-          </h3>
-          <div className="o-form__matrix-2">
-            <span className="form-control-wrap your-name is-required">
-              <input
-                type="text"
-                name="your-name"
-                value-size="40"
-                className="o-form__control"
-                placeholder="Navn"
-              />
-            </span>
-            <span className="form-control-wrap your-email is-required">
-              <input
-                type="email"
-                name="your-email"
-                value-size="40"
-                className="o-form__control"
-                placeholder="E-post"
-              />
-            </span>
-            <span className="form-control-wrap your-name">
-              <input
-                type="text"
-                name="your-phone"
-                value-size="40"
-                className="o-form__control"
-                placeholder="Mobilnummer (valgfritt)"
-              />
-            </span>
-            <span className="form-control-wrap your-location">
-              <input
-                type="tel"
-                name="your-location"
-                value-size="40"
-                className="o-form__control"
-                placeholder="Beliggenhet (valgfritt)"
-              />
-            </span>
-          </div>
-        </div>
-
-        <div className="o-form__group">
-          <span className="form-control-wrap message">
-            <textarea
-              name="message"
-              cols="40"
-              rows="6"
-              className="o-form__control textarea is-required o-form__control"
-              placeholder="Fortell oss hva vi kan hjelpe deg med"
-            ></textarea>
-          </span>
-        </div>
-
-        <div className="o-form__group">
-          <span className="form-control-wrap gdpr-contact-consent">
-            <span className="list-item is-required">
-              <label>
-                <input
-                  type="checkbox"
-                  name="checkbox-gdpr-contact-consent"
-                  value="1"
-                />
-
-                <span className="list-item-label">
-                  Jeg godtar SquareRoot's
-                  <a href="terms-and-conditions">&nbsp;vilkår og betingelser</a>
-                  .
+      {isSending ? (
+        <Loading />
+      ) : (
+        <>
+          {sent && (
+            <Alert
+              severity="success"
+              onClose={() => {
+                setSent(false);
+              }}
+            >
+              <AlertTitle>
+                <strong>Email sent!</strong>
+              </AlertTitle>
+              We will get back to you as soon as possible.
+            </Alert>
+          )}
+          <form>
+            <div className={pathname === "/contact-us" ? "o-form__group" : "o-form__group u-txt--center"}>
+              <div className="o-form__matrix-2">
+                <span className="form-control-wrap your-name is-required">
+                  <input
+                    type="text"
+                    name="name"
+                    value-size="40"
+                    className="o-form__control"
+                    placeholder="Navn"
+                    value={values.name || ""}
+                    onChange={handleChange}
+                  />
+                  {errors.name && (
+                    <p className="help is-danger">{errors.name}</p>
+                  )}
                 </span>
-              </label>
-            </span>
-          </span>
-
-          <span className="form-control-wrap subscribe-contact">
-            <span className="list-item">
-              <label>
-                <input
-                  type="checkbox"
-                  name="checkbox-subscribe-contact"
-                  value="1"
-                />
-
-                <span className="list-item-label">
-                  {t("contact_page.accept2")}
+                <span className="form-control-wrap your-email is-required">
+                  <input
+                    type="text"
+                    name="email"
+                    value-size="40"
+                    className="o-form__control"
+                    placeholder="E-post"
+                    value={values.email || ""}
+                    onChange={handleChange}
+                  />
+                  {errors.email && (
+                    <p className="help is-danger">{errors.email}</p>
+                  )}
                 </span>
-              </label>
-            </span>
-          </span>
-        </div>
+                <span className="form-control-wrap your-name">
+                  <input
+                    type="text"
+                    name="phone"
+                    value-size="40"
+                    value={values.phone || ""}
+                    onChange={handleChange}
+                    className="o-form__control"
+                    placeholder="Mobilnummer (valgfritt)"
+                  />
+                </span>
+                <span className="form-control-wrap your-location">
+                  <input
+                    type="text"
+                    name="location"
+                    value-size="40"
+                    value={values.location || ""}
+                    onChange={handleChange}
+                    className="o-form__control"
+                    placeholder="Beliggenhet (valgfritt)"
+                  />
+                </span>
+              </div>
+            </div>
 
-        {/*Check if fields "is-required" are valid, if not, then disable button*/}
-        <input
-          type="submit"
-          value="Send"
-          className="submit-form-control submit o-btn"
-          /*disabled*/
-        />
-      </form>
+            <div className={pathname === "/contact-us" ? "o-form__group" : "o-form__group u-txt--center"}>
+              <span className="form-control-wrap message">
+                <textarea
+                  name="message"
+                  cols="40"
+                  rows="6"
+                  className="o-form__control textarea is-required o-form__control"
+                  placeholder="Fortell oss hva du jobber med"
+                  type="text"
+                  value={values.message || ""}
+                  onChange={handleChange}
+                />
+                {errors.message && (
+                  <p className="help is-danger">{errors.message}</p>
+                )}
+              </span>
+            </div>
+
+            <div className={pathname === "/contact-us" ? "o-form__group" : "o-form__group u-txt--center"}>
+              <span className="form-control-wrap gdpr-contact-consent">
+                <span className="list-item is-required">
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="conditions"
+                      value={values.conditions || false}
+                      onChange={handleChange}
+                    />
+
+                    <span className="list-item-label">
+                      Jeg godtar SquareRoot's
+                      <a href="terms-and-conditions">
+                        &nbsp;vilkår og betingelser
+                      </a>
+                      .
+                    </span>
+                  </label>
+                </span>
+              </span>
+              {errors.conditions && (
+                <p className="help is-danger">{errors.conditions}</p>
+              )}
+
+              <span className="form-control-wrap subscribe-contact">
+                <span className="list-item">
+                  <label>
+                    <input
+                      type="checkbox"
+                      name="consent"
+                      value={values.consent || false}
+                      onChange={handleChange}
+                    />
+
+                    <span className="list-item-label">
+                      Jeg ønsker å iblant motta nyhetsbrev fra SquareRoot om
+                      tilbud, nyheter og oppdateringer innen grønn byutvikling.
+                    </span>
+                  </label>
+                </span>
+              </span>
+            </div>
+
+            <button
+              className="submit-form-control submit o-btn"
+              onClick={handleSubmit}
+              disabled={errors.conditions || errors.name || errors.email}
+            >
+              Send
+            </button>
+          </form>
+        </>
+      )}{" "}
     </>
   );
 }
--- square-root/src/pages/frontpages/Partner.js
+++ square-root/src/pages/frontpages/Partner.js
@@ -4,83 +4,51 @@ import Loading from "../../components/Loading";
 import useForm from "../../components/hooks/useForm";
 import validate from "../../components/utility/ContactFormValidation";
 import "../../styles/Partner.css";
-import PartnerContainer from "../../components/PartnerContainer";
+import ContactForm from "../../components/ContactForm";
 import { useTranslation } from "react-i18next";
 
 export default function Partner(props) {
   const { t } = useTranslation();
-  const { values, errors, handleChange, handleSubmit } = useForm(
-    addContact,
-    validate,
-    callback
-  );
-
-  const [isSending, setIsSending] = useState(false);
-  async function addContact() {
-    setIsSending(true);
-    // create request body
-    const data = {
-      body: {
-        name: values.name,
-        email: values.email,
-        message: values.message,
-      },
-    };
-    console.log(data);
-    // call the API gateway
-    const apiData = await API.post("contactFormApi", "/contact", data);
-    console.log({ apiData });
-    setIsSending(false);
-    alert("Mail sent!");
-  }
-
-  async function callback() {
-    console.log(values);
-  }
 
   return (
     <>
-      {isSending ? (
-        <Loading />
-      ) : (
-        <div className="o-container">
-          <div className="c-hero">
-            <h1 className="c-txt--hero">{t("partner_page.title")}</h1>
+      <div className="o-container">
+        <div className="c-hero">
+          <h1 className="c-txt--hero">{t("partner_page.title")}</h1>
+
+          <div className="c-mrg--bottom"></div>
+
+          <div className="c-grid--2x2">
+            <div className="grid-item3 margin-left">
+              <img src="../images/partner.jpg" className="c-img flip-img" />
+            </div>
+
+            <div className="grid-item1">
+              <div className="c-txt-punchline--sm">
+                <h2 className="c-txt-punchline--sm">
+                  {t("partner_page.subtitle")}
+                </h2>
+                {t("partner_page.d1")}
+                <br />
+                <br />
+                {t("partner_page.d2")}
+              </div>
+            </div>
 
             <div className="c-mrg--bottom"></div>
-
-            <div className="c-grid--2x2">
-              <div className="grid-item3 margin-left">
-                <img src="../images/partner.jpg" className="c-img flip-img" />
-              </div>
-
-              <div className="grid-item1">
-                <div className="c-txt-punchline--sm">
-                  <h2 className="c-txt-punchline--sm">
-                    {t("partner_page.subtitle")}
-                  </h2>
-                  {t("partner_page.d1")}
-                  <br />
-                  <br />
-                  {t("partner_page.d2")}
-                </div>
-              </div>
-
-              <div className="c-mrg--bottom"></div>
-            </div>
-            <div className="c-section c-section--cta background-grey">
-              <div className="o-container">
-                <h2 className="c-txt-sectiontitle--sm u-txt--center">
-                  {t("partner_page.contact")}
-                </h2>
-                <div className="u-txt--center">
-                  <PartnerContainer />
-                </div>
+          </div>
+          <div className="c-section c-section--cta background-grey">
+            <div className="o-container">
+              <h2 className="c-txt-sectiontitle--sm u-txt--center">
+                {t("partner_page.contact")}
+              </h2>
+              <div className="u-txt--center">
+                <ContactForm />
               </div>
             </div>
           </div>
         </div>
-      )}{" "}
+      </div>
     </>
   );
 }
