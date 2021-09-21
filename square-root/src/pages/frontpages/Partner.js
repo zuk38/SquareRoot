3fc3efbdd173aac107c17d5ff410197ee7b4edad
gro97 <gro.oleivsgard@gmail.com>
2021-09-21 23:46:17 +0200

    Added grid for partner punchlines

--- square-root/src/pages/frontpages/Partner.js
+++ square-root/src/pages/frontpages/Partner.js
@@ -43,7 +43,7 @@ export default function Partner(props) {
 
                         <div className="c-mrg--bottom"></div>
 
-                        <div className="c-grid--2x2">
+                        <div className="c-grid--2x1">
                             <div className="grid-item3 margin-left">
                                 <img src="../images/partner.jpg" className="c-img flip-img"/>
                             </div>
@@ -54,8 +54,6 @@ export default function Partner(props) {
                                 <div className="c-txt-punchline--sm">
                                     <h2 className="c-txt-punchline--sm">
                                         Sammen gjør vi byene våres grønne. 
-                                                      
-                                                            Bokstavelig talt.
                                     </h2>
                                     Vi ønsker å samarbeide med alle aktører i planleggingen og
                                                                                       utformingen av urbane grøntområder. Dette er for at vi kan gjøre arbeidsprosessen sømløs slik at du
@@ -70,10 +68,46 @@ export default function Partner(props) {
 
                                 </div>
                             </div>
-
-
-                            <div className="c-mrg--bottom"></div>
+                           
                         </div>
+
+                        <div className="o-container--lrg background-grey">
+<div className="c-grid--4x1">
+
+    <div className="grid-item1">
+        
+    <div className="c-txt-punchline--sm">Real Estate Developer</div>
+    <div className="short-border"></div>
+    <p>
+    See the concepts that will green you building
+    </p>
+    </div>
+
+    <div className="grid-item3">
+    <div className="c-txt-punchline--sm">Landscape Architect</div>
+    
+    <div className="short-border"></div><p>
+    See what is available and make your own amazing project
+    </p>
+    </div>
+
+    <div className="grid-item9">
+    <div className="c-txt-punchline--sm">Landscape Entrepreneur</div>
+    <div className="short-border"></div>
+    <p>
+    Order the right plant for the right spot on our platform
+    </p>
+    </div>
+
+    <div className="grid-item11">
+    <div className="c-txt-punchline--sm">Plant nursery</div>
+    <div className="short-border"></div>
+    <p>
+    Show and sell your plants on our platform 
+    </p>
+    </div>
+</div>
+</div>
                         <div className="c-section c-section--cta background-grey">
                             <div className="o-container">
                                 <h2 className="c-txt-sectiontitle--sm u-txt--center">
