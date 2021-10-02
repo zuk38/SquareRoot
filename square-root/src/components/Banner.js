fa6b194053a1275beafc4a709df6efc872904ff9
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-10-02 11:56:52 +0200

    translated error page and fixed design

--- square-root/src/components/Banner.js
+++ square-root/src/components/Banner.js
@@ -1,18 +1,13 @@
 import React from "react";
 
 export default function Banner({ children, img, title }) {
-  return title ? (
-    <div className="errorBanner">
-      <h1>{title}</h1>
-      <div/>
-      {children}
-    </div>
-  ) : (
-    <div className="banner">
-      <div className="top-left">
-        <img src={img} />
+  return (
+    <header className="defaultHero">
+      <div className="errorBanner">
+        <h1>{title}</h1>
+        <div />
+        {children}
       </div>
-      <div className="bottom-right">{children}</div>
-    </div>
+    </header>
   );
 }
