dd66d2f88ed130cb5f45ef12b88dd7f59f462a20
gro97 <gro.oleivsgard@gmail.com>
2021-10-24 17:00:08 +0200

    Formatted code

--- square-root/platform/src/components/forms/fb-elements/SignUpDropdown.js
+++ square-root/platform/src/components/forms/fb-elements/SignUpDropdown.js
@@ -1,64 +1,65 @@
 import React from 'react';
 import CustomSelect from '../custom-elements/CustomSelect';
 import CustomFormLabel from '../custom-elements/CustomFormLabel';
-import { MenuItem } from '@mui/material';
+import {MenuItem} from '@mui/material';
 
 const titles = [
     {
-      value: 'RED',
-      label: 'Real Estate Developer',
+        value: 'RED',
+        label: 'Real Estate Developer'
     },
     {
-      value: 'LARK',
-      label: 'Landscape Architect',
+        value: 'LARK',
+        label: 'Landscape Architect'
     },
     {
-      value: 'LENT',
-      label: 'Landscape Entrepreneur',
+        value: 'LENT',
+        label: 'Landscape Entrepreneur'
     },
     {
-      value: 'PN',
-      label: 'Plant Nursery',
+        value: 'PN',
+        label: 'Plant Nursery'
+    }, {
+        value: 'other',
+        label: 'Other'
     },
-    {
-      value: 'other',
-      label: 'Other',
-    },
-  ];
+];
 
-  const SignUpDropdown = () => {
-    
+const SignUpDropdown = () => {
 
 
     const [title, setTitle] = React.useState('');
 
     const handleChange = (event) => {
-      setTitle(event.target.value);
+        setTitle(event.target.value);
     };
 
 
-return (
-    <>
-    <CustomFormLabel htmlFor="standard-select-number">I am a...</CustomFormLabel>
-            <CustomSelect
-              fullWidth
-              id="standard-select-number"
-              variant="outlined"
-              value={title}
-              onChange={handleChange}
-              size="small"
-              sx={{
-                mb: 3,
-              }}
-            >
-              {titles.map((option) => (
-                <MenuItem key={option.value} value={option.value}>
-                  {option.label}
-                </MenuItem>
-              ))}
-            </CustomSelect>
-    </>
-);
+    return (
+        <>
+            <CustomFormLabel htmlFor="standard-select-number">I am a...</CustomFormLabel>
+            <CustomSelect fullWidth id="standard-select-number" variant="outlined"
+                value={title}
+                onChange={handleChange}
+                size="small"
+                sx={
+                    {mb: 3}
+            }>
+                {
+                titles.map((option) => (
+                    <MenuItem key={
+                            option.value
+                        }
+                        value={
+                            option.value
+                    }>
+                        {
+                        option.label
+                    } </MenuItem>
+                ))
+            } </CustomSelect>
+        </>
+    );
 };
 
 export default SignUpDropdown;
