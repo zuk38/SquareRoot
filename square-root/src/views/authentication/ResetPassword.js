0a50ba95ef979df609335d71912c51374465bae2
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-10-31 12:33:53 +0100

    Refactor reset password: add a view to submit a verification code and a new password

--- square-root/src/views/authentication/ResetPassword.js
+++ square-root/src/views/authentication/ResetPassword.js
@@ -1,136 +1,78 @@
-import React from 'react';
-import { Grid, Box, Typography, Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Grid, Box } from '@mui/material';
 import img1 from '../../assets/images/backgrounds/login-bg.svg';
-
 import LogoIcon from '../../layouts/full-layout/logo/LogoIcon';
-import CustomTextField from '../../components/forms/custom-elements/CustomTextField';
-import CustomFormLabel from '../../components/forms/custom-elements/CustomFormLabel';
-
 import PageContainer from '../../components/container/PageContainer';
+import ResetPassCode from '../../components/auth/ResetPassCode';
+import ResetPassEmail from '../../components/auth/ResetPassEmail';
 
-const ResetPassword = () => (
-  <PageContainer title="Reset Password" description="this is Reset Password page">
-    <Grid container spacing={0} sx={{ height: '100vh', justifyContent: 'center' }}>
+export default function ResetPassword() {
+  const [emailProvided, setEmailProvided] = useState(false);
+
+  return (
+    <PageContainer
+      title='Reset Password'
+      description='this is Reset Password page'
+    >
       <Grid
-        item
-        xs={12}
-        sm={12}
-        lg={6}
-        sx={{
-          background: (theme) => `${theme.palette.mode === 'dark' ? '#1c1f25' : '#ffffff'}`,
-        }}
+        container
+        spacing={0}
+        sx={{ height: '100vh', justifyContent: 'center' }}
       >
-        <Box
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          lg={6}
           sx={{
-            position: 'relative',
+            background: (theme) =>
+              `${theme.palette.mode === 'dark' ? '#1c1f25' : '#ffffff'}`,
           }}
         >
           <Box
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
             sx={{
-              position: {
-                xs: 'relative',
-                lg: 'absolute',
-              },
-              height: { xs: 'auto', lg: '100vh' },
-              right: { xs: 'auto', lg: '-50px' },
-              margin: '0 auto',
+              position: 'relative',
             }}
           >
-            <img
-              src={img1}
-              alt="bg"
-              style={{
-                width: '100%',
-                maxWidth: '812px',
-              }}
-            />
-          </Box>
-
-          <Box
-            display="flex"
-            alignItems="center"
-            sx={{
-              p: 4,
-              position: 'absolute',
-              top: '0',
-            }}
-          >
-            <LogoIcon />
-          </Box>
-        </Box>
-      </Grid>
-      <Grid item xs={12} sm={8} lg={6} display="flex" alignItems="center">
-        <Grid container spacing={0} display="flex" justifyContent="center">
-          <Grid item xs={12} lg={9} xl={6}>
             <Box
+              display='flex'
+              alignItems='center'
+              justifyContent='center'
               sx={{
-                p: 4,
+                position: {
+                  xs: 'relative',
+                  lg: 'absolute',
+                },
+                height: { xs: 'auto', lg: '100vh' },
+                right: { xs: 'auto', lg: '-50px' },
+                margin: '0 auto',
               }}
             >
-              <Typography variant="h2" fontWeight="700">
-                Forgot your password?
-              </Typography>
-
-              <Typography
-                color="textSecondary"
-                variant="h5"
-                fontWeight="400"
-                sx={{
-                  mt: 2,
+              <img
+                src={img1}
+                alt='bg'
+                style={{
+                  width: '100%',
+                  maxWidth: '812px',
                 }}
-              >
-                Please enter the email address associated with your account and We will email you a
-                link to reset your password.
-              </Typography>
-
-              <Box
-                sx={{
-                  mt: 4,
-                }}
-              >
-                <CustomFormLabel htmlFor="reset-email">Email Adddress</CustomFormLabel>
-                <CustomTextField id="reset-email" variant="outlined" fullWidth />
-
-                <Button
-                  color="secondary"
-                  variant="contained"
-                  size="large"
-                  fullWidth
-                  component={Link}
-                  to="/"
-                  sx={{
-                    pt: '10px',
-                    pb: '10px',
-                    mt: 4,
-                  }}
-                >
-                  Reset Password
-                </Button>
-                <Button
-                  color="secondary"
-                  size="large"
-                  fullWidth
-                  component={Link}
-                  to="/auth/login"
-                  sx={{
-                    pt: '10px',
-                    pb: '10px',
-                    mt: 2,
-                  }}
-                >
-                  Back to Login
-                </Button>
-              </Box>
+              />
             </Box>
-          </Grid>
-        </Grid>
-      </Grid>
-    </Grid>
-  </PageContainer>
-);
 
-export default ResetPassword;
+            <Box
+              display='flex'
+              alignItems='center'
+              sx={{
+                p: 4,
+                position: 'absolute',
+                top: '0',
+              }}
+            >
+              <LogoIcon />
+            </Box>
+          </Box>
+        </Grid>
+        {!emailProvided ? <ResetPassEmail redirectToCode={(arg) => setEmailProvided(arg)}/> : <ResetPassCode />}
+      </Grid>
+    </PageContainer>
+  );
+}
