06124aaccccee78921133f1c5d4562280826b957
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-11-02 23:22:57 +0100

    Refactor Login functional component

--- square-root/src/views/authentication/Login.js
+++ square-root/src/views/authentication/Login.js
@@ -21,224 +21,226 @@ import PageContainer from '../../components/container/PageContainer';
 import img1 from '../../assets/images/backgrounds/login-bg-transp.png';
 import LogoIcon from '../../layouts/full-layout/logo/LogoIcon';
 
-const Login = () => (
-  <PageContainer title='Login' description='this is Login page'>
-    <Grid
-      container
-      spacing={0}
-      sx={{ height: '100vh', justifyContent: 'center' }}
-    >
+import { checkEmail, checkPassword } from '../../utility/Validation';
+
+export default function Login() {
+  const [values, setValues] = useState({});
+  const [errors, setErrors] = useState({});
+
+  return (
+    <PageContainer title='Login' description='this is Login page'>
       <Grid
-        item
-        xs={12}
-        sm={12}
-        lg={6}
-        sx={{
-          background: (theme) =>
-            `${theme.palette.mode === 'dark' ? '#1c1f25' : '#ffffff'}`,
-        }}
+        container
+        spacing={0}
+        sx={{ height: '100vh', justifyContent: 'center' }}
       >
-        <Box
+        <Grid
+          item
+          xs={12}
+          sm={12}
+          lg={6}
           sx={{
-            position: 'relative',
+            background: (theme) =>
+              `${theme.palette.mode === 'dark' ? '#1c1f25' : '#ffffff'}`,
           }}
         >
           <Box
-            display='flex'
-            alignItems='center'
-            justifyContent='center'
             sx={{
-              position: {
-                xs: 'relative',
-                lg: 'absolute',
-              },
-              height: { xs: 'auto', lg: '100vh' },
-              right: { xs: 'auto', lg: '-50px' },
-              margin: '0 auto',
+              position: 'relative',
             }}
           >
-            <img
-              src={img1}
-              alt='bg'
-              style={{
-                width: '100%',
-                maxWidth: '812px',
+            <Box
+              display='flex'
+              alignItems='center'
+              justifyContent='center'
+              sx={{
+                position: {
+                  xs: 'relative',
+                  lg: 'absolute',
+                },
+                height: { xs: 'auto', lg: '100vh' },
+                right: { xs: 'auto', lg: '-50px' },
+                margin: '0 auto',
               }}
-            />
-          </Box>
+            >
+              <img
+                src={img1}
+                alt='bg'
+                style={{
+                  width: '100%',
+                  maxWidth: '812px',
+                }}
+              />
+            </Box>
 
-          <Box
-            sx={{
-              p: 4,
-              position: 'absolute',
-              top: '0',
-            }}
-          >
-            <LogoIcon />
-          </Box>
-        </Box>
-      </Grid>
-      <Grid item xs={12} sm={8} lg={6} display='flex' alignItems='center'>
-        <Grid container spacing={0} display='flex' justifyContent='center'>
-          <Grid item xs={12} lg={9} xl={6}>
             <Box
               sx={{
                 p: 4,
+                position: 'absolute',
+                top: '0',
               }}
             >
-              <Typography variant='secondary'>
-                Welcome to SquareRoot
-              </Typography>
-              <Box display='flex' alignItems='center'>
-                <Typography
-                  color='textSecondary'
-                  variant='h6'
-                  fontWeight='500'
-                  sx={{
-                    mr: 1,
-                  }}
-                >
-                  New to SquareRoot?
-                </Typography>
-                <Typography
-                  component={Link}
-                  to='/auth/register'
-                  fontWeight='500'
-                  sx={{
-                    display: 'block',
-                    textDecoration: 'none',
-                    color: 'primary.main',
-                  }}
-                >
-                  Create an account
-                </Typography>
-              </Box>
+              <LogoIcon />
+            </Box>
+          </Box>
+        </Grid>
+        <Grid item xs={12} sm={8} lg={6} display='flex' alignItems='center'>
+          <Grid container spacing={0} display='flex' justifyContent='center'>
+            <Grid item xs={12} lg={9} xl={6}>
               <Box
                 sx={{
-                  mt: 4,
+                  p: 4,
                 }}
               >
-                <CustomFormLabel htmlFor='email'>Email Address</CustomFormLabel>
-                <CustomTextField id='email' variant='outlined' fullWidth />
-                <CustomFormLabel htmlFor='password'>Password</CustomFormLabel>
-                <CustomTextField
-                  id='password'
-                  type='password'
-                  variant='outlined'
-                  fullWidth
-                  sx={{
-                    mb: 3,
-                  }}
-                />
+                <Typography variant='secondary'>
+                  Welcome to SquareRoot
+                </Typography>
+                <Box display='flex' alignItems='center'>
+                  <Typography
+                    color='textSecondary'
+                    variant='h6'
+                    fontWeight='500'
+                    sx={{
+                      mr: 1,
+                    }}
+                  >
+                    New to SquareRoot?
+                  </Typography>
+                  <Typography
+                    component={Link}
+                    to='/auth/register'
+                    fontWeight='500'
+                    sx={{
+                      display: 'block',
+                      textDecoration: 'none',
+                      color: 'primary.main',
+                    }}
+                  >
+                    Create an account
+                  </Typography>
+                </Box>
                 <Box
                   sx={{
-                    display: {
-                      xs: 'block',
-                      sm: 'flex',
-                      lg: 'flex',
-                    },
-                    alignItems: 'center',
+                    mt: 4,
                   }}
                 >
-                  <FormGroup>
-                    <FormControlLabel
-                      control={<CustomCheckbox defaultChecked />}
-                      label='Remember this Device'
-                      sx={{
-                        mb: 2,
-                      }}
-                    />
-                  </FormGroup>
+                  <CustomFormLabel htmlFor='email'>
+                    Email Address
+                  </CustomFormLabel>
+                  <CustomTextField id='email' variant='outlined' fullWidth />
+                  <CustomFormLabel htmlFor='password'>Password</CustomFormLabel>
+                  <CustomTextField
+                    id='password'
+                    type='password'
+                    variant='outlined'
+                    fullWidth
+                    sx={{
+                      mb: 3,
+                    }}
+                  />
                   <Box
                     sx={{
-                      ml: 'auto',
+                      display: {
+                        xs: 'block',
+                        sm: 'flex',
+                        lg: 'flex',
+                      },
+                      alignItems: 'center',
+                    }}
+                  >
+                    <FormGroup>
+                      <FormControlLabel
+                        control={<CustomCheckbox defaultChecked />}
+                        label='Remember this Device'
+                        sx={{
+                          mb: 2,
+                        }}
+                      />
+                    </FormGroup>
+                    <Box
+                      sx={{
+                        ml: 'auto',
+                      }}
+                    >
+                      <Typography
+                        component={Link}
+                        to='/auth/reset-password'
+                        fontWeight='500'
+                        sx={{
+                          display: 'block',
+                          textDecoration: 'none',
+                          mb: '16px',
+                          color: 'primary.main',
+                        }}
+                      >
+                        Forgot Password?
+                      </Typography>
+                    </Box>
+                  </Box>
+
+                  <Button
+                    color='secondary'
+                    variant='contained'
+                    size='large'
+                    fullWidth
+                    component={Link}
+                    to='/'
+                    sx={{
+                      pt: '10px',
+                      pb: '10px',
+                    }}
+                  >
+                    Sign In
+                  </Button>
+                  <Box
+                    sx={{
+                      position: 'relative',
+                      textAlign: 'center',
+                      mt: '20px',
+                      mb: '20px',
+                      '&::before': {
+                        content: '""',
+                        background: (theme) =>
+                          `${
+                            theme.palette.mode === 'dark'
+                              ? '#42464d'
+                              : '#ecf0f2'
+                          }`,
+                        height: '1px',
+                        width: '100%',
+                        position: 'absolute',
+                        left: '0',
+                        top: '13px',
+                      },
                     }}
                   >
                     <Typography
-                      component={Link}
-                      to='/auth/reset-password'
-                      fontWeight='500'
+                      component='span'
+                      color='textSecondary'
+                      variant='h6'
+                      fontWeight='400'
                       sx={{
-                        display: 'block',
-                        textDecoration: 'none',
-                        mb: '16px',
-                        color: 'primary.main',
+                        position: 'relative',
+                        padding: '0 12px',
+                        background: (theme) =>
+                          `${
+                            theme.palette.mode === 'dark' ? '#282c34' : '#fff'
+                          }`,
                       }}
                     >
-                      Forgot Password?
+                      or sign in with
                     </Typography>
                   </Box>
-                </Box>
 
-                <Button
-                  color='secondary'
-                  variant='contained'
-                  size='large'
-                  fullWidth
-                  component={Link}
-                  to='/'
-                  sx={{
-                    pt: '10px',
-                    pb: '10px',
-                  }}
-                >
-                  Sign In
-                </Button>
-                <Box
-                  sx={{
-                    position: 'relative',
-                    textAlign: 'center',
-                    mt: '20px',
-                    mb: '20px',
-                    '&::before': {
-                      content: '""',
-                      background: (theme) =>
-                        `${
-                          theme.palette.mode === 'dark' ? '#42464d' : '#ecf0f2'
-                        }`,
-                      height: '1px',
-                      width: '100%',
-                      position: 'absolute',
-                      left: '0',
-                      top: '13px',
-                    },
-                  }}
-                >
-                  <Typography
-                    component='span'
-                    color='textSecondary'
-                    variant='h6'
-                    fontWeight='400'
-                    sx={{
-                      position: 'relative',
-                      padding: '0 12px',
-                      background: (theme) =>
-                        `${theme.palette.mode === 'dark' ? '#282c34' : '#fff'}`,
-                    }}
-                  >
-                    or sign in with
-                  </Typography>
-                </Box>
-
-                <Box>
-                  <Button
-                    variant='outlined'
-                    size='large'
-                    display='flex'
-                    alignitems='center'
-                    justifycontent='center'
-                    sx={{
-                      width: '100%',
-                      borderColor: (theme) =>
-                        `${
-                          theme.palette.mode === 'dark' ? '#42464d' : '#dde3e8'
-                        }`,
-                      borderWidth: '2px',
-                      textAlign: 'center',
-                      mt: 2,
-                      pt: '10px',
-                      pb: '10px',
-                      '&:hover': {
+                  <Box>
+                    <Button
+                      variant='outlined'
+                      size='large'
+                      display='flex'
+                      alignitems='center'
+                      justifycontent='center'
+                      sx={{
+                        width: '100%',
                         borderColor: (theme) =>
                           `${
                             theme.palette.mode === 'dark'
@@ -246,39 +248,50 @@ const Login = () => (
                               : '#dde3e8'
                           }`,
                         borderWidth: '2px',
-                      },
-                    }}
-                  >
-                    <Box display='flex' alignItems='center'>
-                      <GoogleIcon
-                        sx={{
-                          color: (theme) => theme.palette.error.main,
-                        }}
-                      />
-                      <Typography
-                        variant='h6'
-                        sx={{
-                          ml: 1,
-                          color: (theme) =>
+                        textAlign: 'center',
+                        mt: 2,
+                        pt: '10px',
+                        pb: '10px',
+                        '&:hover': {
+                          borderColor: (theme) =>
                             `${
                               theme.palette.mode === 'dark'
-                                ? theme.palette.grey.A200
-                                : '#13152a'
+                                ? '#42464d'
+                                : '#dde3e8'
                             }`,
-                        }}
-                      >
-                        Google
-                      </Typography>
-                    </Box>
-                  </Button>
+                          borderWidth: '2px',
+                        },
+                      }}
+                    >
+                      <Box display='flex' alignItems='center'>
+                        <GoogleIcon
+                          sx={{
+                            color: (theme) => theme.palette.error.main,
+                          }}
+                        />
+                        <Typography
+                          variant='h6'
+                          sx={{
+                            ml: 1,
+                            color: (theme) =>
+                              `${
+                                theme.palette.mode === 'dark'
+                                  ? theme.palette.grey.A200
+                                  : '#13152a'
+                              }`,
+                          }}
+                        >
+                          Google
+                        </Typography>
+                      </Box>
+                    </Button>
+                  </Box>
                 </Box>
               </Box>
-            </Box>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
-  </PageContainer>
-);
-
-export default Login;
+    </PageContainer>
+  );
+}
