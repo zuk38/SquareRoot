80ef392cd4a4d1b238ff6b783869d203ffcea5db
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-11-02 23:20:09 +0100

    Refactor useForm hook

--- square-root/src/components/hooks/useForm.js
+++ square-root/src/components/hooks/useForm.js
@@ -1,81 +1,34 @@
-import { useState, useEffect } from "react";
+import { useState } from 'react';
 
-const useForm = (callback, validate, action) => {
-  const [values, setValues] = useState({ role: "Real Estate Developer" });
+const useForm = (
+  initialFormValues = {},
+  validateOnChange = false,
+  validate
+) => {
+  const [values, setValues] = useState(initialFormValues);
   const [errors, setErrors] = useState({});
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const [triedSubmitting, setTriedSubmitting] = useState(false);
-  const [edit, setEdit] = useState({});
 
-  useEffect(() => {
-    if (Object.keys(errors).length === 0 && isSubmitting) {
-      callback();
-      setTriedSubmitting(false);
-      setValues({
-        name: undefined,
-        email: undefined,
-        message: undefined,
-        phone: undefined,
-        role: undefined,
-      });
-    }
-  }, [errors]);
-
-  useEffect(() => {
-    if (triedSubmitting) setErrors(validate(values, edit)); //update errors every time values change
-  }, [values]);
-
-  const handleSubmit = async (event, editMode) => {
-    if (event) event.preventDefault();
-    setEdit(editMode);
-    await action();
-    setValues((values) => {
-      setErrors(validate(values, editMode)); //update errors
-      setTriedSubmitting(true);
-      setIsSubmitting(true);
-      return values;
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues({
+      ...values,
+      [name]: value,
     });
+    if (validateOnChange) validate({ [name]: value });
   };
 
-  const handleChange = (event) => {
-    event.persist();
-    const value = event.target.type === "checkbox" ? event.target.checked : event.target.value;
-    setValues((values) => ({
-      ...values,
-      [event.target.name]: value,
-    }));
-    setValues((values) => ({
-      ...values,
-      cognito: null,
-    })); //update
-    setIsSubmitting(false);
+  const resetForm = () => {
+    setValues(initialFormValues);
+    setErrors({});
   };
-
-  const handleDropDownChange = (event) => {
-    setValues({ ...values, role: event.target.value }); //update role
-  };
-
-  const setCity = (city) => {
-    if (city) {
-      setValues({ ...values, city: city });
-      setErrors({zip: undefined})
-    }
-    else setErrors({ ...errors, zip: "Invalid zip code" });
-  };
-
-  const setProjectExistsErrors = (exist) => {
-    if(exist) setErrors({...errors, name: "Project with that name already exists!"})
-    else setErrors({...errors, name: ""})
-  }
 
   return {
-    handleChange,
-    handleSubmit,
-    handleDropDownChange,
-    setCity,
-    setProjectExistsErrors,
     values,
+    setValues,
     errors,
+    setErrors,
+    handleChange,
+    resetForm
   };
 };
 
