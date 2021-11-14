5444fa60e87e41eeb16a4521065e77fca7e8fe08
Karolina Maciejewska <kmaciejewska14@gmail.com>
2021-11-14 21:42:10 +0100

    Refactor select dropdown to be reusable

--- square-root/src/components/forms/custom-elements/SelectDropdown.js
+++ square-root/src/components/forms/custom-elements/SelectDropdown.js
@@ -42,42 +42,27 @@ CustomThumbComponent.propTypes = {
   children: PropTypes.node,
 };
 
-const SelectDropdown = () => {
-  const [age, setAge] = React.useState('1');
-  const [select1, setSelect] = React.useState('1');
-  const [select2, setSelect2] = React.useState('1');
-
-  const handleChange = (event) => {
-    setAge(event.target.value);
-  };
-  const handleChange4 = (event2) => {
-    setSelect(event2.target.value);
-  };
-
-  const handleChange5 = (event3) => {
-    setSelect2(event3.target.value);
-  };
-
-  const [value, setValue] = React.useState(null);
-  const [value2, setValue2] = React.useState(null);
-
-  const [value3, setValue3] = React.useState(30);
-  const handleChange6 = (event, newValue) => {
-    setValue3(newValue);
-  };
+const SelectDropdown = (props) => {
   return (
     <>
+      <CustomFormLabel htmlFor='standard-select-number'>
+        {props.label}
+      </CustomFormLabel>
       <CustomSelect
         labelId='demo-simple-select-label'
         id='demo-simple-select'
-        value={age}
-        onChange={handleChange}
+        value={props.value}
+        onChange={props.handleChange}
         fullWidth
         size='small'
+        variant={props.variant}
+        sx={{ mb: props.mb }}
       >
-        <MenuItem value={1}>One</MenuItem>
-        <MenuItem value={2}>Two</MenuItem>
-        <MenuItem value={3}>Three</MenuItem>
+        {props.options.map((option) => (
+          <MenuItem key={option} value={option}>
+            {option}
+          </MenuItem>
+        ))}
       </CustomSelect>
     </>
   );
