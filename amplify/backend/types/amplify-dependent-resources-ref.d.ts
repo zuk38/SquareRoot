00d19fa111e58c8952c833daca111d69ee97ff89
Karolina Maciejewska <kmaciejewska14@gmail.com>
2022-02-05 11:30:33 +0100

    sync updates from amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,59 +1,59 @@
 export type AmplifyDependentResourcesAttributes = {
-  auth: {
-    squarerootprojectfe3d4090: {
-      IdentityPoolId: 'string';
-      IdentityPoolName: 'string';
-      UserPoolId: 'string';
-      UserPoolArn: 'string';
-      UserPoolName: 'string';
-      AppClientIDWeb: 'string';
-      AppClientID: 'string';
-      CreatedSNSRole: 'string';
-    };
-  };
-  function: {
-    addUserToGroup: {
-      Name: 'string';
-      Arn: 'string';
-      Region: 'string';
-      LambdaExecutionRole: 'string';
-    };
-    contactFormFunction: {
-      Name: 'string';
-      Arn: 'string';
-      Region: 'string';
-      LambdaExecutionRole: 'string';
-    };
-    sendEmail: {
-      Name: 'string';
-      Arn: 'string';
-      Region: 'string';
-      LambdaExecutionRole: 'string';
-    };
-  };
-  storage: {
-    squareRootImages: {
-      BucketName: 'string';
-      Region: 'string';
-    };
-    contactFormTable: {
-      Name: 'string';
-      Arn: 'string';
-      StreamArn: 'string';
-      PartitionKeyName: 'string';
-      PartitionKeyType: 'string';
-      Region: 'string';
-    };
-  };
-  api: {
-    squarerootproject: {
-      GraphQLAPIIdOutput: 'string';
-      GraphQLAPIEndpointOutput: 'string';
-    };
-    contactFormApi: {
-      RootUrl: 'string';
-      ApiName: 'string';
-      ApiId: 'string';
-    };
-  };
-};
+    "auth": {
+        "squarerootprojectfe3d4090": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string",
+            "CreatedSNSRole": "string"
+        }
+    },
+    "function": {
+        "addUserToGroup": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "contactFormFunction": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "sendEmail": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "storage": {
+        "squareRootImages": {
+            "BucketName": "string",
+            "Region": "string"
+        },
+        "contactFormTable": {
+            "Name": "string",
+            "Arn": "string",
+            "StreamArn": "string",
+            "PartitionKeyName": "string",
+            "PartitionKeyType": "string",
+            "Region": "string"
+        }
+    },
+    "api": {
+        "squarerootproject": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        },
+        "contactFormApi": {
+            "RootUrl": "string",
+            "ApiName": "string",
+            "ApiId": "string"
+        }
+    }
+}
